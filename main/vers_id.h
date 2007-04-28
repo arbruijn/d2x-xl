970e76e41bb4cd2546e0e5be774e32b295d9015e
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2007-04-28 00:02:13 +0000

    Finalized v1.9.182.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@430 01821946-851b-0410-b7f4-f1477c70e57a

--- main/vers_id.h
+++ main/vers_id.h
@@ -8,7 +8,7 @@
 #define _VERS_ID
 
 #ifndef VERSION
-#	define VERSION "1.9.181"
+#	define VERSION "1.9.182"
 #endif
 
 #define D2X_NAME		"D2X-XL "
@@ -20,7 +20,7 @@
 #define D2X_MINOR		9
 #endif
 #ifndef D2X_MICRO
-#define D2X_MICRO		181
+#define D2X_MICRO		182
 #endif
 
 #define VERSION_TYPE		"Full Version"
