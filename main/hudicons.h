f0a4599993c210bb1fc3c9043dcac19da2fd0fa9
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2008-02-04 14:40:25 +0000

    Added hudicons.h.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@1138 01821946-851b-0410-b7f4-f1477c70e57a

---
+++ main/hudicons.h
@@ -0,0 +1,9 @@
+#ifndef _HUDICONS_H
+#define _HUDICONS_H
+
+void FreeInventoryIcons (void);
+void FreeObjTallyIcons (void);
+void HUDShowIcons (void);
+//	-----------------------------------------------------------------------------
+
+#endif //_HUDICONS_H
