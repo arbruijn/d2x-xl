56a8c8b6ff4c41e8a1419cac7d128c8d0f3da45f
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2008-04-25 14:50:10 +0000

    The mine was distorted for all cockpit types except the regular fullscreen view.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@1439 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -9,7 +9,7 @@
 
 #ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.12.85"
+#		define VERSION		"1.12.86"
 #	endif
 #endif
 
@@ -22,7 +22,7 @@
 #define D2X_MINOR		12
 #endif
 #ifndef D2X_MICRO
-#define D2X_MICRO		85
+#define D2X_MICRO		86
 #endif
 
 #define VERSION_TYPE		"Full Version"
