de6ca9ddff7f79bfc27c293c5549bc11f7ff1d28
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2008-08-15 22:19:47 +0000

    Raised depth radius for soft smoke particles.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@1992 01821946-851b-0410-b7f4-f1477c70e57a

--- effects/particles.cpp
+++ effects/particles.cpp
@@ -759,7 +759,7 @@ if (iBuffer) {
 			if (gameData.render.lights.dynamic.headlights.nLights && !(gameStates.render.automap.bDisplay || gameData.smoke.nLastType))
 				G3SetupHeadlightShader (1, 0, &color);
 			else if (gameOpts->render.effects.bSoftParticles && !gameData.smoke.nLastType)
-				LoadGlareShader (5);
+				LoadGlareShader (10);
 			else if (gameStates.render.history.nShader >= 0) {
 				glUseProgramObject (0);
 				gameStates.render.history.nShader = -1;
