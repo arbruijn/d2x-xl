dba685a65dd1cc269e1e4e00cd61a1641e7861ef
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-01-11 19:20:00 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@2679 01821946-851b-0410-b7f4-f1477c70e57a

--- network/netmisc-new.cpp
+++ network/netmisc-new.cpp
@@ -23,7 +23,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "pstypes.h"
 #include "mono.h"
 
-#if 1 || defined(WORDS_BIGENDIAN) || defined(__BIG_ENDIAN__)
+#if defined(WORDS_BIGENDIAN) || defined(__BIG_ENDIAN__)
 
 #include "byteswap.h"
 #include "segment.h"
