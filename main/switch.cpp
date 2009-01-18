8c3fe84c6c2a6e81012871ce3ebe9a2d611c76a9
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-01-18 23:29:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@2871 01821946-851b-0410-b7f4-f1477c70e57a

--- main/switch.cpp
+++ main/switch.cpp
@@ -36,6 +36,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "light.h"
 #include "textdata.h"
 #include "marker.h"
+#include "state.h"
 
 #ifdef EDITOR
 #include "editor/editor.h"
