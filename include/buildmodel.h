c05dc349337ae498798d5c10d238f15e97a3ecdd
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-01-22 00:48:14 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@2954 01821946-851b-0410-b7f4-f1477c70e57a

--- include/buildmodel.h
+++ include/buildmodel.h
@@ -23,7 +23,8 @@ int G3BuildModelFromPOF (CObject *objP, int nModel, CPolyModel *pp, CBitmap **mo
 
 static inline int IsDefaultModel (int nModel)
 {
-return gameData.models.polyModels [0][nModel].DataSize () == gameData.models.polyModels [1][nModel].DataSize ();
+return (nModel < gameData.models.nPolyModels) &&
+		 (gameData.models.polyModels [0][nModel].DataSize () == gameData.models.polyModels [1][nModel].DataSize ());
 }
 
 //------------------------------------------------------------------------------
