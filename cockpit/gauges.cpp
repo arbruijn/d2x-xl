684ae7cda0d719312e8ef62a6e3dae33b9625657
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-01-23 20:56:57 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@2992 01821946-851b-0410-b7f4-f1477c70e57a

--- cockpit/gauges.cpp
+++ cockpit/gauges.cpp
@@ -1600,11 +1600,19 @@ HUDBitBlt (GAUGE_AFTERBURNER, AFTERBURNER_GAUGE_X, AFTERBURNER_GAUGE_Y);
 CCanvas::Current ()->SetColorRGB (0, 0, 0, 255);
 if ((yMax = FixMul (I2X (1) - nEnergy, AFTERBURNER_GAUGE_H))) {
 	y [0] = y [1] = HUD_SCALE_Y (AFTERBURNER_GAUGE_Y);
-	y [2] = y [3] = HUD_SCALE_Y (AFTERBURNER_GAUGE_Y + yMax);
-	x [0] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [0]);
-	x [1] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [1]);
-	x [2] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [2 * yMax - 2]);
-	x [3] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [2 * yMax - 1]);
+	y [3] = HUD_SCALE_Y (AFTERBURNER_GAUGE_Y + yMax) - 1;
+	x [1] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [0]);
+	x [0] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [1] + 1);
+	x [2] = x [1];
+	y [2] = 0;
+	for (int i = 1; i < yMax - 1; i++)
+		if (x [2] >= tableP [2 * i]) {
+			x [2] = tableP [2 * i];
+			y [2] = i;
+			}
+	x [2] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + x [2] + 1);
+	y [2] = HUD_SCALE_X (AFTERBURNER_GAUGE_Y + y [2]);
+	x [3] = HUD_SCALE_X (AFTERBURNER_GAUGE_X + tableP [2 * yMax - 1] + 1);
 	gameStates.render.grAlpha = FADE_LEVELS;
 	OglDrawFilledPoly (x, y, 4);
 	}
