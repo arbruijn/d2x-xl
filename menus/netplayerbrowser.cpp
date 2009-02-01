fa0e472d198deb33a1bb1d7ca54cab12a5129a70
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-02-01 15:11:07 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@3312 01821946-851b-0410-b7f4-f1477c70e57a

--- menus/netplayerbrowser.cpp
+++ menus/netplayerbrowser.cpp
@@ -175,8 +175,8 @@ if (gameOpts->multi.bNoRankings)
 else
 	sprintf (text [0], "%d. %s%-20s", 1, pszRankStrings [netPlayers.players [gameData.multiplayer.nLocalPlayer].rank], LOCALPLAYER.callsign);
 m.AddCheck (text [0], 0);
-for (i = 0; i < MAX_PLAYERS + 4; i++) {
-	sprintf (text [i], "%d.  %-20s", i+1, "");
+for (i = 1; i < MAX_PLAYERS + 4; i++) {
+	sprintf (text [i], "%d.  %-20s", i + 1, "");
 	m.AddCheck (text [i], 0);
 	}
 sprintf (title, "%s %d %s", TXT_TEAM_SELECT, gameData.multiplayer.nMaxPlayers, TXT_TEAM_PRESS_ENTER);
