11135319949d160f877e2daa2b69bbb6f47e7953
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-02-01 09:48:39 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@3308 01821946-851b-0410-b7f4-f1477c70e57a

--- main/gamefolders.cpp
+++ main/gamefolders.cpp
@@ -263,7 +263,7 @@ if (*gameFolders.szHomeDir) {
 	}
 #endif
 if (*gameFolders.szHomeDir) {
-#if defined(__macosx__)
+#ifdef __macosx__
 	char *pszOSXCacheDir = GetMacOSXCacheFolder ();
 	sprintf (gameFolders.szTextureCacheDir [0], "%s/%s",pszOSXCacheDir, TEXTUREDIR_D2);
 	CFile::MkDir (gameFolders.szTextureCacheDir [0]);
@@ -280,7 +280,8 @@ if (*gameFolders.szHomeDir) {
 	sprintf (gameFolders.szCacheDir, "%s/%s/64", pszOSXCacheDir, CACHEDIR);
 	CFile::MkDir (gameFolders.szCacheDir);
 	sprintf (gameFolders.szCacheDir, "%s/%s", pszOSXCacheDir, CACHEDIR);
-#elif defined (__unix__)
+#else
+#	ifdef __unix__
 	sprintf (szDataRootDir, "%s/.d2x-xl", gameFolders.szHomeDir);
 #	else
 	strcpy (szDataRootDir, gameFolders.szHomeDir);
