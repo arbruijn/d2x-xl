e7b726edde83efddc371dc73ad5f49cdcb7230e8
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-02-02 23:41:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@3362 01821946-851b-0410-b7f4-f1477c70e57a

--- gameio/piggy.cpp
+++ gameio/piggy.cpp
@@ -452,7 +452,9 @@ if (gameData.pig.tex.nHamFileVersion < 3) {
 cf.Close ();
 /*---*/PrintLog ("      Looking for Descent 1 data files\n");
 strcpy (szD1PigFileName, "descent.pig");
-if (!cfPiggy [1].File ())
+if (cfPiggy [1].File ())
+	cfPiggy [1].Seek (0, SEEK_SET);
+else
 	cfPiggy [1].Open (szD1PigFileName, gameFolders.szDataDir, "rb", 0);
 if (cfPiggy [1].File ()) {
 	gameStates.app.bHaveD1Data = 1;
