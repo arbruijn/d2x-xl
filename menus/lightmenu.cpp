b9de629f33bd6c32b51cd8ff0b3378761100f955
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-03-20 13:35:17 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@4208 01821946-851b-0410-b7f4-f1477c70e57a

--- menus/lightmenu.cpp
+++ menus/lightmenu.cpp
@@ -199,13 +199,13 @@ do {
 		lightOpts.nLightmaps = m.AddSlider (szSlider + 1, gameOpts->render.nLightmapQuality, 0, 4, KEY_Q, HTX_LMAP_QUALITY);
 
 		if (nLighting == 3) {
-			sprintf (szSlider + 1, TXT_MAX_PASSES_PER_FACE, nPasses);
-			*szSlider = *(TXT_MAX_PASSES_PER_FACE - 1);
-			lightOpts.nLights = m.AddSlider (szSlider + 1, nPasses - 1, 0,  15, KEY_P, HTX_MAX_PASSES_PER_FACE);
-
 			sprintf (szSlider + 1, TXT_MAX_LIGHTS_PER_PASS, gameOpts->ogl.nMaxLightsPerPass);
 			*szSlider = *(TXT_MAX_LIGHTS_PER_PASS - 1);
 			lightOpts.nPasses = m.AddSlider (szSlider + 1, gameOpts->ogl.nMaxLightsPerPass - 1, 0, 7, KEY_S, HTX_MAX_LIGHTS_PER_PASS);
+
+			sprintf (szSlider + 1, TXT_MAX_PASSES_PER_FACE, nPasses);
+			*szSlider = *(TXT_MAX_PASSES_PER_FACE - 1);
+			lightOpts.nLights = m.AddSlider (szSlider + 1, nPasses - 1, 0, min (15, 32 / gameOpts->ogl.nMaxLightsPerPass), KEY_P, HTX_MAX_PASSES_PER_FACE);
 			}
 		}
 	optFlickerLights = m.AddCheck (TXT_FLICKERLIGHTS, extraGameInfo [0].bFlickerLights, KEY_F, HTX_FLICKERLIGHTS);
