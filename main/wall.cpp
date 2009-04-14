4d35dbd18d72b0fd23bae2d814ce8282eaf5be54
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-04-14 11:47:48 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@4570 01821946-851b-0410-b7f4-f1477c70e57a

--- main/wall.cpp
+++ main/wall.cpp
@@ -649,8 +649,7 @@ if (cloakWallP->time > CLOAKING_WALL_TIME) {
 else if (SHOW_DYN_LIGHT || (cloakWallP->time > CLOAKING_WALL_TIME / 2)) {
 	int oldType = frontWallP->nType;
 #if 1
-	float c = float (FADE_LEVELS) * X2F (cloakWallP->time);
-	frontWallP->cloakValue = fix (c * c + 0.5f);
+	frontWallP->cloakValue = fix (float (FADE_LEVELS) * X2F (cloakWallP->time));
 #else
 	if (SHOW_DYN_LIGHT)
 		frontWallP->cloakValue = fix (FADE_LEVELS * X2F (cloakWallP->time) + 0.5f);
