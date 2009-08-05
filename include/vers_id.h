e8b7f2f2823c33859bf799034576521586544d16
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-08-05 09:37:36 +0000

    Ambient sounds weren't played anymore.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@5037 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -8,7 +8,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.14.81"
+#		define VERSION		"1.14.82"
 #	endif
 //#endif
 
@@ -21,7 +21,7 @@
 #define D2X_MINOR		14
 #endif
 #ifndef D2X_MICRO
-#define D2X_MICRO		81
+#define D2X_MICRO		82
 #endif
 
 #define VERSION_TYPE		"Full Version"
