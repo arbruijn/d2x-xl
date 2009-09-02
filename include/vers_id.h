61319ade6909ab32bd9cf78254b471e25c4f90fe
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-09-02 07:35:09 +0000

    D2X-XL was considering descent2.p11 and descent2.p22 vital and was terminated when it couldn't find them, but actually they aren't.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@5285 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -8,7 +8,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.14.114"
+#		define VERSION		"1.14.115"
 #	endif
 //#endif
 
@@ -21,7 +21,7 @@
 #define D2X_MINOR		14
 #endif
 #ifndef D2X_MICRO
-#define D2X_MICRO		114
+#define D2X_MICRO		115
 #endif
 
 #define VERSION_TYPE		"Full Version"
