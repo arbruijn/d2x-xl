06e906662d7dec21587f07b67a15defeead2fc40
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-09-24 14:51:07 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@5436 01821946-851b-0410-b7f4-f1477c70e57a

--- main/descent.cpp
+++ main/descent.cpp
@@ -239,9 +239,9 @@ GrPrintF (NULL, 0x8000, CCanvas::Current ()->Height () - 3 * h - 6, "Press F1 fo
 fontManager.SetColorRGBi (RGB_PAL (31, 31, 31), 1, 0, 0);
 CCanvas::Current ()->SetColor (CCanvas::Current ()->FontColor (0));
 OglDrawLine (CCanvas::Current ()->Width () / 2 - 275,
-		CCanvas::Current ()->Height () - 2 * h - 4,
+		CCanvas::Current ()->Height () - 2 * h - 5,
 		CCanvas::Current ()->Width () / 2 + 275,
-		CCanvas::Current ()->Height () - 2 * h - 4,
+		CCanvas::Current ()->Height () - 2 * h - 5,
 		NULL);
 #if 0
 OglDrawLine (2, //CCanvas::Current ()->Width () / 2 - 200,
