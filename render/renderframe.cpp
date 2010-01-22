6706763662b9dbbf40979a8f58f4fd71af0e578c
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2010-01-22 00:06:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@5813 01821946-851b-0410-b7f4-f1477c70e57a

--- render/renderframe.cpp
+++ render/renderframe.cpp
@@ -422,7 +422,7 @@ if (gameOpts->render.cockpit.bGuidedInMainView && GuidedMissileActive ()) {
 		}
   	gameData.objs.viewerP = gameData.objs.guidedMissile [gameData.multiplayer.nLocalPlayer].objP;
 	UpdateRenderedData (0, gameData.objs.viewerP, 0, 0);
-	if (cameraManager.Render ())
+	if ((nEyeOffset <= 0) && cameraManager.Render ())
 		CCanvas::SetCurrent (&gameStates.render.vr.buffers.subRender [0]);
 	RenderFrame (nEyeOffset, 0);
 	if (nEyeOffset <= 0)
