2b2e4be87ea08c0c08f006e5d0ed1c5382f2d5c2
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2010-01-26 23:48:10 +0000

    Fixed anaglyph 3D reticle display. Bumped version number up.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@5843 01821946-851b-0410-b7f4-f1477c70e57a

--- cockpit/genericcockpit.cpp
+++ cockpit/genericcockpit.cpp
@@ -1878,7 +1878,7 @@ fontManager.SetCurrent (GAME_FONT);
 
 bool bLimited = (gameStates.render.bRearView || gameStates.render.bChaseCam || gameStates.render.bFreeCam);
 
-if (gameOpts->render.cockpit.bReticle && !gameStates.app.bPlayerIsDead && !transformation.m_info.bUsePlayerHeadAngles && !ogl.StereoSeparation ())
+if (gameOpts->render.cockpit.bReticle && !gameStates.app.bPlayerIsDead && !transformation.m_info.bUsePlayerHeadAngles && !gameOpts->render.n3DGlasses)
 	DrawReticle (0);
 
 if (!bLimited) {
--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.14.206"
+#		define VERSION		"1.14.207"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #define D2X_MINOR		14
 #endif
 #ifndef D2X_MICRO
-#define D2X_MICRO		206
+#define D2X_MICRO		207
 #endif
 
 #define VERSION_TYPE		"Full Version"
