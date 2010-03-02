ce757b8553e3e70c3722ce2cae89541523077ef8
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2010-03-02 14:43:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@6215 01821946-851b-0410-b7f4-f1477c70e57a

--- render/lighttrails.cpp
+++ render/lighttrails.cpp
@@ -102,70 +102,6 @@ if ((IsEnergyPowerup (objP->info.nId) ? gameOpts->render.coronas.bPowerups : gam
 
 // -----------------------------------------------------------------------------
 
-void RenderTowedFlag (CObject *objP)
-{
-	static CFloatVector fVerts [4] = {
-		CFloatVector::Create(0.0f, 2.0f / 3.0f, 0.0f, 1.0f),
-		CFloatVector::Create(0.0f, 2.0f / 3.0f, -1.0f, 1.0f),
-		CFloatVector::Create(0.0f, -(1.0f / 3.0f), -1.0f, 1.0f),
-		CFloatVector::Create(0.0f, -(1.0f / 3.0f), 0.0f, 1.0f)
-	};
-
-	static tTexCoord2f texCoordList [2][4] = {
-		{{{0.0f, -0.3f}}, {{1.0f, -0.3f}}, {{1.0f, 0.7f}}, {{0.0f, 0.7f}}},
-		{{{0.0f, 0.7f}}, {{1.0f, 0.7f}}, {{1.0f, -0.3f}}, {{0.0f, -0.3f}}}
-		};
-
-if (gameStates.app.bNostalgia)
-	return;
-if (SHOW_SHADOWS && (gameStates.render.nShadowPass != 1))
-	return;
-if (IsTeamGame && (gameData.multiplayer.players [objP->info.nId].flags & PLAYER_FLAGS_FLAG)) {
-		CFixVector		vPos = objP->info.position.vPos;
-		CFloatVector	vPosf, verts [4];
-		tFlagData		*pf = gameData.pig.flags + !GetTeam (objP->info.nId);
-		tPathPoint		*pp = pf->path.GetPoint ();
-		int				i, bStencil;
-		float				r;
-		CBitmap		*bmP;
-
-	if (pp) {
-		bStencil = ogl.StencilOff ();
-		ogl.SelectTMU (GL_TEXTURE0);
-		ogl.SetTexturing (true);
-		glTexEnvf (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-		LoadBitmap (pf->bmi.index, 0);
-		bmP = gameData.pig.tex.bitmapP + pf->vcP->frames [pf->vci.nCurFrame].index;
-		bmP->SetTranspType (2);
-		vPos += objP->info.position.mOrient.FVec () * (-objP->info.xSize);
-		r = X2F (objP->info.xSize);
-		transformation.Begin (vPos, pp->mOrient);
-		glColor3f (1.0f, 1.0f, 1.0f);
-		for (i = 0; i < 4; i++) {
-			vPosf [X] = 0;
-			vPosf [Y] = fVerts [i][Y] * r;
-			vPosf [Z] = fVerts [i][Z] * r;
-			transformation.Transform (verts [i], vPosf, 0);
-			}
-		bmP->SetTexCoord (texCoordList [0]);
-		ogl.RenderQuad (bmP, verts, 3);
-		for (i = 3; i >= 0; i--) {
-			vPosf [X] = 0;
-			vPosf [Y] = fVerts [i][Y] * r;
-			vPosf [Z] = fVerts [i][Z] * r;
-			transformation.Transform (verts [3 - i], vPosf, 0);
-			}
-		bmP->SetTexCoord (texCoordList [1]);
-		ogl.RenderQuad (bmP, verts, 3);
-		transformation.End ();
-		ogl.BindTexture (0);
-		ogl.StencilOn (bStencil);
-		}
-	}
-}
-
-// -----------------------------------------------------------------------------
-
 void RenderLaserCorona (CObject *objP, tRgbaColorf *colorP, float alpha, float fScale)
 {
 	int	bAdditive = 1; //gameOpts->render.bAdditive
@@ -454,25 +390,6 @@ else if ((objP->info.nType == OBJ_DEBRIS) && gameOpts->render.nDebrisLife) {
 			}
 		RenderWeaponCorona (objP, &debrisGlow, h, 5 * objP->info.xSize, 1.5f, 1, 1, 0);
 		}
-	}
-}
-
-//------------------------------------------------------------------------------
-
-fix flashDist = F2X (0.9f);
-
-//create flash for CPlayerData appearance
-void CObject::CreateAppearanceEffect (void)
-{
-	CFixVector	vPos = info.position.vPos;
-
-if (this == gameData.objs.viewerP)
-	vPos += info.position.mOrient.FVec () * FixMul (info.xSize, flashDist);
-CObject* effectObjP = /*Object*/CreateExplosion (info.nSegment, vPos, info.xSize, VCLIP_PLAYER_APPEARANCE);
-if (effectObjP) {
-	effectObjP->info.position.mOrient = info.position.mOrient;
-	if (gameData.eff.vClips [0][VCLIP_PLAYER_APPEARANCE].nSound > -1)
-		audio.CreateObjectSound (gameData.eff.vClips [0][VCLIP_PLAYER_APPEARANCE].nSound, SOUNDCLASS_PLAYER, OBJ_IDX (effectObjP));
 	}
 }
 
---
+++ render/rendershields.cpp
@@ -0,0 +1,297 @@
+/*
+THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
+SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
+END-USERS, AND SUBJECT TO ALL OF THE TERMS AND CONDITIONS HEREIN, GRANTS A
+ROYALTY-FREE, PERPETUAL LICENSE TO SUCH END-USERS FOR USE BY SUCH END-USERS
+IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
+SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
+FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
+CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
+AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
+COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <conf.h>
+#endif
+
+#include <string.h>	// for memset
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#include "descent.h"
+#include "newdemo.h"
+#include "network.h"
+#include "interp.h"
+#include "ogl_lib.h"
+#include "rendermine.h"
+#include "transprender.h"
+#include "glare.h"
+#include "sphere.h"
+#include "marker.h"
+#include "fireball.h"
+#include "objsmoke.h"
+#include "objrender.h"
+#include "objeffects.h"
+#include "hiresmodels.h"
+#include "hitbox.h"
+
+#ifndef fabsf
+#	define fabsf(_f)	(float) fabs (_f)
+#endif
+
+//------------------------------------------------------------------------------
+
+void TransformHitboxf (CObject *objP, CFloatVector *vertList, int iSubObj)
+{
+
+	CFloatVector		hv;
+	tHitbox*				phb = gameData.models.hitboxes [objP->rType.polyObjInfo.nModel].hitboxes + iSubObj;
+	CFixVector			vMin = phb->vMin;
+	CFixVector			vMax = phb->vMax;
+	int					i;
+
+for (i = 0; i < 8; i++) {
+	hv [X] = X2F (hitBoxOffsets [i][X] ? vMin [X] : vMax [X]);
+	hv [Y] = X2F (hitBoxOffsets [i][Y] ? vMin [Y] : vMax [Y]);
+	hv [Z] = X2F (hitBoxOffsets [i][Z] ? vMin [Z] : vMax [Z]);
+	transformation.Transform (vertList [i], hv, 0);
+	}
+}
+
+//------------------------------------------------------------------------------
+
+#if RENDER_HITBOX
+
+void RenderHitbox (CObject *objP, float red, float green, float blue, float alpha)
+{
+if (objP->rType.polyObjInfo.nModel < 0)
+	return;
+
+	CFloatVector	v;
+	tHitbox*			pmhb = gameData.models.hitboxes [objP->rType.polyObjInfo.nModel].hitboxes.Buffer ();
+	tCloakInfo		ci = {0, FADE_LEVELS, 0, 0, 0, 0, 0};
+	int				i, j, iBox, nBoxes, bHit = 0;
+	float				fFade;
+
+if (!SHOW_OBJ_FX)
+	return;
+if (objP->info.nType == OBJ_PLAYER) {
+	if (!EGI_FLAG (bPlayerShield, 0, 1, 0))
+		return;
+	if (gameData.multiplayer.players [objP->info.nId].flags & PLAYER_FLAGS_CLOAKED) {
+		if (!GetCloakInfo (objP, 0, 0, &ci))
+			return;
+		fFade = (float) ci.nFadeValue / (float) FADE_LEVELS;
+		red *= fFade;
+		green *= fFade;
+		blue *= fFade;
+		}
+
+	}
+else if (objP->info.nType == OBJ_ROBOT) {
+	if (!(gameOpts->render.effects.bEnabled && gameOpts->render.effects.bRobotShields))
+		return;
+	if (objP->cType.aiInfo.CLOAKED) {
+		if (!GetCloakInfo (objP, 0, 0, &ci))
+			return;
+		fFade = (float) ci.nFadeValue / (float) FADE_LEVELS;
+		red *= fFade;
+		green *= fFade;
+		blue *= fFade;
+		}
+	}
+if (!EGI_FLAG (nHitboxes, 0, 0, 0)) {
+	DrawShieldSphere (objP, red, green, blue, alpha, 1);
+	return;
+	}
+else if (extraGameInfo [IsMultiGame].nHitboxes == 1) {
+	iBox =
+	nBoxes = 0;
+	}
+else {
+	iBox = 1;
+	nBoxes = gameData.models.hitboxes [objP->rType.polyObjInfo.nModel].nHitboxes;
+	}
+ogl.SetDepthMode (GL_LEQUAL);
+ogl.SetBlending (true);
+ogl.SetBlendMode (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+ogl.SetTexturing (false);
+ogl.SetDepthWrite (false);
+
+tBox hb [MAX_HITBOXES + 1];
+TransformHitboxes (objP, &objP->info.position.vPos, hb);
+
+//transformation.Begin (objP->info.position.vPos, objP->info.position.mOrient);
+for (; iBox <= nBoxes; iBox++) {
+#if 0
+	if (iBox)
+		transformation.Begin (pmhb [iBox].vOffset, CAngleVector::ZERO);
+	TransformHitboxf (objP, vertList, iBox);
+#endif
+	glColor4f (red, green, blue, alpha / 2);
+	glBegin (GL_QUADS);
+	for (i = 0; i < 6; i++) {
+		for (j = 0; j < 4; j++) {
+			v.Assign (hb [iBox].faces [i].v [j]);
+			transformation.Transform (v, v, 0);
+			glVertex3fv (reinterpret_cast<GLfloat*> (&v));
+		//	glVertex3fv (reinterpret_cast<GLfloat*> (vertList + hitboxFaceVerts [i][j]));
+			}
+		}
+	glEnd ();
+	glLineWidth (3);
+	glColor4f (red, green, blue, alpha);
+	for (i = 0; i < 6; i++) {
+		CFixVector c;
+		c.SetZero ();
+		glColor4f (red, green, blue, alpha / 2);
+		glBegin (GL_LINES);
+		for (j = 0; j < 4; j++) {
+			c += hb [iBox].faces [i].v [j];
+			v.Assign (hb [iBox].faces [i].v [j]);
+			transformation.Transform (v, v, 0);
+			glVertex3fv (reinterpret_cast<GLfloat*> (&v));
+			//glVertex3fv (reinterpret_cast<GLfloat*> (vertList + hitboxFaceVerts [i][j]));
+			}
+		c /= I2X (4);
+		v.Assign (c);
+		glColor4f (1.0f, 0.5f, 0.0f, alpha);
+		transformation.Transform (v, v, 0);
+		glVertex3fv (reinterpret_cast<GLfloat*> (&v));
+		v.Assign (c + hb [iBox].faces [i].n [1] * I2X (5));
+		transformation.Transform (v, v, 0);
+		glVertex3fv (reinterpret_cast<GLfloat*> (&v));
+		glEnd ();
+		}
+	glLineWidth (1);
+//	if (iBox)
+//		transformation.End ();
+	}
+//transformation.End ();
+float r = X2F (CFixVector::Dist (pmhb->vMin, pmhb->vMax) / 2);
+#if 0 //DBG //display collision point
+if (gameStates.app.nSDLTicks - gameData.models.hitboxes [objP->rType.polyObjInfo.nModel].tHit < 500) {
+	CObject	o;
+
+	o.info.position.vPos = gameData.models.hitboxes [objP->rType.polyObjInfo.nModel].vHit;
+	o.info.position.mOrient = objP->info.position.mOrient;
+	o.info.xSize = I2X (2);
+	objP->rType.polyObjInfo.nModel = -1;
+	//SetRenderView (0, NULL);
+	DrawShieldSphere (&o, 1, 0, 0, 0.33f, 1);
+	}
+#endif
+ogl.SetDepthWrite (true);
+ogl.SetDepthMode (GL_LESS);
+}
+
+#endif
+
+// -----------------------------------------------------------------------------
+
+void RenderPlayerShield (CObject *objP)
+{
+	int			bStencil, dt = 0, i = objP->info.nId, nColor = 0;
+	float			alpha, scale = 1;
+	tCloakInfo	ci;
+
+	static tRgbaColorf shieldColors [3] = {{0, 0.5f, 1, 1}, {1, 0.5f, 0, 1}, {1, 0.8f, 0.6f, 1}};
+
+if (!SHOW_OBJ_FX)
+	return;
+if (SHOW_SHADOWS &&
+	 (FAST_SHADOWS ? (gameStates.render.nShadowPass != 1) : (gameStates.render.nShadowPass != 3)))
+	return;
+if (EGI_FLAG (bPlayerShield, 0, 1, 0)) {
+	if (gameData.multiplayer.players [i].flags & PLAYER_FLAGS_CLOAKED) {
+		if (!GetCloakInfo (objP, 0, 0, &ci))
+			return;
+		scale = (float) ci.nFadeValue / (float) FADE_LEVELS;
+		scale *= scale;
+		}
+	bStencil = ogl.StencilOff ();
+	gameData.render.shield.SetPulse (gameData.multiplayer.spherePulse + i);
+	if (gameData.multiplayer.bWasHit [i]) {
+		if (gameData.multiplayer.bWasHit [i] < 0) {
+			gameData.multiplayer.bWasHit [i] = 1;
+			gameData.multiplayer.nLastHitTime [i] = gameStates.app.nSDLTicks;
+			SetupSpherePulse (gameData.multiplayer.spherePulse + i, 0.1f, 0.5f);
+			dt = 0;
+			}
+		else if ((dt = gameStates.app.nSDLTicks - gameData.multiplayer.nLastHitTime [i]) >= 300) {
+			gameData.multiplayer.bWasHit [i] = 0;
+			SetupSpherePulse (gameData.multiplayer.spherePulse + i, 0.02f, 0.4f);
+			}
+		}
+#if !RENDER_HITBOX
+	if (gameOpts->render.effects.bOnlyShieldHits && !gameData.multiplayer.bWasHit [i])
+		return;
+#endif
+	if (gameData.multiplayer.players [i].flags & PLAYER_FLAGS_INVULNERABLE)
+		nColor = 2;
+	else if (gameData.multiplayer.bWasHit [i])
+		nColor = 1;
+	else
+		nColor = 0;
+	if (gameData.multiplayer.bWasHit [i]) {
+		alpha = (gameOpts->render.effects.bOnlyShieldHits ? (float) cos (sqrt ((double) dt / float (SHIELD_EFFECT_TIME)) * Pi / 2) : 1);
+		scale *= alpha;
+		}
+	else if (gameData.multiplayer.players [i].flags & PLAYER_FLAGS_INVULNERABLE)
+		alpha = 1;
+	else {
+		alpha = X2F (gameData.multiplayer.players [i].shields) / 100.0f;
+		scale *= alpha;
+		if (gameData.multiplayer.spherePulse [i].fSpeed == 0.0f)
+			SetupSpherePulse (gameData.multiplayer.spherePulse + i, 0.02f, 0.5f);
+		}
+#if RENDER_HITBOX
+	RenderHitbox (objP, shieldColors [nColor].red * scale, shieldColors [nColor].green * scale, shieldColors [nColor].blue * scale, alpha);
+#else
+	DrawShieldSphere (objP, shieldColors [nColor].red * scale, shieldColors [nColor].green * scale, shieldColors [nColor].blue * scale, alpha, 1);
+#endif
+	ogl.StencilOn (bStencil);
+	}
+}
+
+// -----------------------------------------------------------------------------
+
+void RenderRobotShield (CObject *objP)
+{
+	static tRgbaColorf shieldColors [3] = {{0.75f, 0, 0.75f, 1}, {0, 0.5f, 1},{1, 0.1f, 0.25f, 1}};
+
+#if RENDER_HITBOX
+RenderHitbox (objP, 0.5f, 0.0f, 0.6f, 0.4f);
+#else
+#endif
+#if 1
+	float			scale = 1;
+	tCloakInfo	ci;
+	fix			dt;
+
+if (!(gameOpts->render.effects.bEnabled && gameOpts->render.effects.bRobotShields))
+	return;
+if ((objP->info.nType == OBJ_ROBOT) && objP->cType.aiInfo.CLOAKED) {
+	if (!GetCloakInfo (objP, 0, 0, &ci))
+		return;
+	scale = (float) ci.nFadeValue / (float) FADE_LEVELS;
+	scale *= scale;
+	}
+dt = gameStates.app.nSDLTicks - objP->TimeLastHit ();
+if (dt < SHIELD_EFFECT_TIME) {
+	scale *= gameOpts->render.effects.bOnlyShieldHits ? float (cos (sqrt (float (dt) / float (SHIELD_EFFECT_TIME)) * Pi / 2)) : 1;
+	DrawShieldSphere (objP, shieldColors [2].red * scale, shieldColors [2].green * scale, shieldColors [2].blue * scale, 0.5f * scale, 1);
+	}
+else if (!gameOpts->render.effects.bOnlyShieldHits) {
+	if ((objP->info.nType != OBJ_ROBOT) || ROBOTINFO (objP->info.nId).companion)
+		DrawShieldSphere (objP, 0.0f, 0.5f * scale, 1.0f * scale, objP->Damage () / 2 * scale, 1);
+	else
+		DrawShieldSphere (objP, 0.75f * scale, 0.0f, 0.75f * scale, objP->Damage () / 2 * scale, 1);
+	}
+#endif
+}
+
+//------------------------------------------------------------------------------
+//eof
