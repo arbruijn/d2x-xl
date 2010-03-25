897fc24acf9808f11601461972599abeafc369e0
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2010-03-25 16:54:25 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@6671 01821946-851b-0410-b7f4-f1477c70e57a

--- effects/particles.cpp
+++ effects/particles.cpp
@@ -983,13 +983,13 @@ if ((m_nType == BUBBLE_PARTICLES) && gameOpts->render.particles.bWiggleBubbles)
 	vCenter [X] += (float) sin (nFrame / 4.0f * Pi) / (10 + rand () % 6);
 if (m_bRotate && gameOpts->render.particles.bRotate) {
 	int i = (m_nOrient & 1) ? 63 - m_nRotFrame : m_nRotFrame;
-	CFixMatrix mOrient = gameData.render.mine.viewer.position.mOrient * mRot [i];
+	CFixMatrix mOrient = gameData.render.mine.viewer.mOrient * mRot [i];
 	uVec.Assign (mOrient.UVec ());
 	rVec.Assign (mOrient.RVec ());
 	}
 else {
-	uVec.Assign (gameData.render.mine.viewer.position.mOrient.UVec ());
-	rVec.Assign (gameData.render.mine.viewer.position.mOrient.RVec ());
+	uVec.Assign (gameData.render.mine.viewer.mOrient.UVec ());
+	rVec.Assign (gameData.render.mine.viewer.mOrient.RVec ());
 	}
 uVec *= m_nHeight * fScale;
 rVec *= m_nWidth * fScale;
