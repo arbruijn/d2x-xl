4a92cdc423453b644a0e273e0d6c4663607d1fbd
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2010-10-12 23:54:59 +0000

    Fixed: OpenMP versions of the game could hang when rendering lightning. Improved: You can now chose two glow quality settings (standard will clip the output area, high will render over the entire screen)
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@8416 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.15.91"
+#		define VERSION		"1.15.92"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #	define D2X_MINOR	15
 #endif
 #ifndef D2X_MICRO
-#	define D2X_MICRO	91
+#	define D2X_MICRO	92
 #endif
 
 #define VERSION_TYPE		"Full Version"
