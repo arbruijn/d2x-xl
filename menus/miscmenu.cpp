679d9eaef2d023c7e3bdca9aa71e0554fad76d36
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-02-21 19:30:00 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@8646 01821946-851b-0410-b7f4-f1477c70e57a

--- menus/miscmenu.cpp
+++ menus/miscmenu.cpp
@@ -202,7 +202,7 @@ do {
 		miscOpts.nScreenshots = m.AddSlider (szSlider + 1, gameOpts->app.nScreenShotInterval, 0, 7, KEY_S, HTX_MISC_SCREENSHOTS);  
 
 		m.AddText ("", 0);
-		miscOpts.nAutoDl = m.AddCheck (TXT_DL_ENABLE, extraGameInfo [0].bAutoDownload, KEY_A, HTX_MISC_MISSIONDL);
+		miscOpts.nAutoDl = m.AddCheck (TXT_DL_ENABLE, extraGameInfo [0].bAutoDownload, KEY_M, HTX_MISC_MISSIONDL);
 		if (extraGameInfo [0].bAutoDownload) {
 			sprintf (szSlider + 1, TXT_DL_TIMEOUT, downloadManager.GetTimeoutSecs ());
 			*szSlider = *(TXT_DL_TIMEOUT - 1);
