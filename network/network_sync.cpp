99bbe85f1a475d88b389a9d6fca135aa0eaeac23
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-02-22 22:34:55 +0000

    Fixed: The game host was flooding the other players with ping requests while waiting for them to join during level transition
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@8651 01821946-851b-0410-b7f4-f1477c70e57a

--- network/network_sync.cpp
+++ network/network_sync.cpp
@@ -678,6 +678,27 @@ while (0 < (size = IpxGetPacketData (packet))) {
 
 //------------------------------------------------------------------------------
 
+class CTimeout {
+	private:
+		time_t	m_t0;
+		time_t	m_duration;
+
+	public:
+		CTimeout (time_t duration = 1000) : m_duration(duration) { Start (); }
+		void Start (time_t t = -1) { m_t0 = (t < 0) ? SDL_GetTicks () : t; }
+		time_t Progress (void) { return m_t0 - SDL_GetTicks (); }
+		bool Expired (bool bRestart = true) {
+			time_t t = SDL_GetTicks ();
+			if (t - m_t0 < m_duration)
+				return false;
+			if (bRestart)
+				Start (t);
+			else
+				m_t0 += m_duration;
+			return true;
+			}
+	};
+
 int NetworkRequestPoll (CMenu& menu, int& key, int nCurItem, int nState)
 {
 if (!IAmGameHost ()) {
@@ -690,11 +711,14 @@ if (nState)
 	int i = 0;
 	int nReady = 0;
 
+static CTimeout to;
 // tell other players that I am here
-for (i = 0; i < gameData.multiplayer.nPlayers; i++) {
-	if (i != gameData.multiplayer.nLocalPlayer) {
-		pingStats [i].launchTime = -1; //TimerGetFixedSeconds ();
-		NetworkSendPing (i); 
+if (to.Expired ()) {
+	for (i = 0; i < gameData.multiplayer.nPlayers; i++) {
+		if (i != gameData.multiplayer.nLocalPlayer) {
+			pingStats [i].launchTime = -1; //TimerGetFixedSeconds ();
+			NetworkSendPing (i); 
+			}
 		}
 	}
 NetworkListen ();
