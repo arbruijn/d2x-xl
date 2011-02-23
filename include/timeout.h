ee5e527f737d0af59bd8f572b750c0a513ce1929
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-02-23 15:59:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@8654 01821946-851b-0410-b7f4-f1477c70e57a

---
+++ include/timeout.h
@@ -0,0 +1,36 @@
+/*
+ * timeout.h
+ *
+ *  Created on: 23.02.2011
+ *      Author: Dietfrid Mali
+ */
+
+#ifndef TIMEOUT_H_
+#define TIMEOUT_H_
+
+class CTimeout {
+	private:
+		time_t	m_t0;
+		time_t	m_duration;
+
+	public:
+		CTimeout (time_t duration = 1000) : m_duration(duration) { Start (); }
+
+		inline void Start (time_t t = -1) { m_t0 = (t < 0) ? SDL_GetTicks () : t; }
+
+		inline time_t Progress (void) { return m_t0 - SDL_GetTicks (); }
+
+		bool Expired (bool bRestart = true) {
+			time_t t = SDL_GetTicks ();
+			if (t - m_t0 < m_duration)
+				return false;
+			if (bRestart)
+				Start (t);
+			else
+				m_t0 += m_duration;
+			return true;
+			}
+	};
+
+
+#endif /* TIMEOUT_H_ */
