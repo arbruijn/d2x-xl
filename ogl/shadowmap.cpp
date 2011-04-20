339665d0fde38cd74d5eabb087c1a4339f6ca2a7
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-04-20 11:46:50 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@9283 01821946-851b-0410-b7f4-f1477c70e57a

--- ogl/shadowmap.cpp
+++ ogl/shadowmap.cpp
@@ -145,129 +145,13 @@ if (gameStates.render.textures.bHaveShadowMapShader && (EGI_FLAG (bShadows, 0, 1
 //------------------------------------------------------------------------------
 
 const char* shadowMapVS = 
-#if 0
-	"varying vec3 position;\r\n" \
-	"varying vec4 vertex;\r\n" \
-	"varying vec3 normal;\r\n" \
-	"void main() {\r\n" \
-	"position = gl_Vertex.xyz;\r\n" \
-	"vertex = gl_ModelViewMatrix * gl_Vertex;\r\n" \
-	"normal = gl_NormalMatrix * gl_Normal;\r\n" \
-	"gl_TexCoord[0] = gl_MultiTexCoord0;\r\n" \
-	"//gl_TexCoord[1] = gl_TextureMatrix[2] * gl_Vertex;\r\n" \
-	"//gl_TexCoord[2] = gl_TextureMatrix[3] * vertex;\r\n" \
-	"//gl_TexCoord[3] = gl_TextureMatrix[4] * vertex;\r\n" \
-	"//gl_TexCoord[4] = gl_TextureMatrix[5] * vertex;\r\n" \
-	"gl_Position = ftransform();\r\n" \
-	"gl_FrontColor = gl_Color;\r\n" \
-	"}\r\n";
-#else
 	"void main() {\r\n" \
 	"gl_TexCoord[0] = gl_MultiTexCoord0;\r\n" \
 	"gl_Position = ftransform();\r\n" \
 	"gl_FrontColor = gl_Color;\r\n" \
 	"}\r\n";
-#endif
 
 const char* shadowMapFS = 
-#if 0
-
-	"uniform sampler2D sceneColor;\r\n" \
-	"uniform sampler2D sceneDepth;\r\n" \
-	"uniform sampler2D/*Shadow*/ shadow0;\r\n" \
-	"//uniform sampler2DShadow shadow1;\r\n" \
-	"//uniform sampler2DShadow shadow2;\r\n" \
-	"//uniform sampler2DShadow shadow3;\r\n" \
-	"uniform int nLights;\r\n" \
-	"//uniform sampler2D lightmask;\r\n" \
-	"varying vec3 position;\r\n" \
-	"varying vec4 vertex;\r\n" \
-	"varying vec3 normal;\r\n" \
-	"varying vec4 shadowCoord;\r\n" \
-	"float Lookup (sampler2DShadow shadowMap, vec2 offset, int i) {\r\n" \
-	"return shadow2DProj (shadowMap, shadowCoord + vec4 (offset.x * shadowCoord.w, offset.y * shadowCoord.w, 0.05, 0.0));\r\n" \
-	"}\r\n" \
-	"float Shadow (sampler2DShadow shadowMap, int i) {\r\n" \
-	"if (i >= nLights)\r\n" \
-	"   return 0.0;\r\n" \
-	"if (vertex.w <= 1.0)\r\n" \
-	"   return 0.0;\r\n" \
-	"return Lookup (shadowMap, vec2 (0.0, 0.0), i);\r\n" \
-	"//float shadow = Lookup (shadowMap, vec2 (-1.5, -1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-1.5, -0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-1.5, 0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-1.5, 1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-0.5, -1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-0.5, -0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-0.5, 0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (-0.5, 1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (0.5, -1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (0.5, -0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (0.5, 0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (0.5, 1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (1.5, -1.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (1.5, -0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (1.5, 0.5, i));\r\n" \
-	"//shadow += Lookup (shadowMap, vec2 (1.5, 1.5, i));\r\n" \
-	"//return shadow / 16.0;\r\n" \
-	"}\r\n" \
-	"void main() {\r\n" \
-	"//float shadow = shadow2DProj (shadow0, gl_TexCoord [1]).r;\r\n" \
-	"float z = texture2D (sceneDepth, gl_TexCoord [0]).r;\r\n" \
-	"float shadowDepth = texture2DProj (shadow0, gl_TexCoord [1]).r;\r\n" \
-	"float light = 0.5 + (z > shadowDepth + 0.0005) ? 0.5 : 0.0;\r\n" \
-	"gl_FragColor = vec4 (texture2D (sceneColor, gl_TexCoord [0]).rgb * light, 1.0);\r\n" \
-	"//gl_FragColor = vec4 (texture2D (shadow0, gl_TexCoord [0]).rgb, 1.0);\r\n" \
-	"//shadow += Shadow (shadow1, 1);\r\n" \
-	"//shadow += Shadow (shadow2, 2);\r\n" \
-	"//shadow += Shadow (shadow3, 3);\r\n" \
-	"//shadow = min (1.0, 0.2 + shadow * 0.8 / (float) nLights);\r\n" \
-	"//gl_FragColor = vec4 (shadow, shadow, shadow, 1.0);\r\n" \
-	"//gl_FragColor = vec4 (texture2D (sceneColor, gl_TexCoord [0]).rgb * shadow, 1.0);\r\n" \
-	"//float s = texture2D (shadow0, gl_TexCoord [0].xy).r;\r\n" \
-	"//gl_FragColor = vec4 (s, s, s, 1.0);\r\n" \
-	"//gl_FragColor = vec4 (vec3 (texture2D (shadow0, gl_TexCoord [0]).r), 1.0);\r\n" \
-	"}\r\n";
-
-#else
-
-#	if 0
-
-	"uniform sampler2D sceneColor;\r\n" \
-	"uniform sampler2D sceneDepth;\r\n" \
-	"uniform sampler2D shadowMap;\r\n" \
-	"uniform mat4 modelviewProjInverse;\r\n" \
-	"uniform vec3 lightPos;\r\n" \
-	"uniform float lightRange;\r\n" \
-	"#define ZNEAR 1.0\r\n" \
-	"#define ZFAR 5000.0\r\n" \
-	"#define A 5001.0 //(ZNEAR + ZFAR)\r\n" \
-	"#define B 4999.0 //(ZNEAR - ZFAR)\r\n" \
-	"#define C 10000.0 //(2.0 * ZNEAR * ZFAR)\r\n" \
-	"#define D (cameraNDC.z * B)\r\n" \
-	"#define ZEYE -10000.0 / (5001.0 + cameraNDC.z * 4999.0) //-(C / (A + D))\r\n" \
-	"void main() {\r\n" \
-	"float fragDepth = texture2D (sceneDepth, gl_TexCoord [0].xy).r;\r\n" \
-	"vec3 cameraNDC = (vec3 (gl_TexCoord [0].xy, fragDepth) - 0.5) * 2.0;\r\n" \
-	"vec4 cameraClipPos;\r\n" \
-	"cameraClipPos.w = -ZEYE;\r\n" \
-	"cameraClipPos.xyz = cameraNDC * cameraClipPos.w;\r\n" \
-	"vec4 lightClipPos = gl_TextureMatrix [2] * cameraClipPos;\r\n" \
-	"float w = abs (lightClipPos.w);\r\n" \
-	"float shadowDepth = ((w < 0.00001) || (abs (lightClipPos.x) > w) || (abs (lightClipPos.y) > w)) ? 2.0 : texture2D (shadowMap, lightClipPos.xy / (lightClipPos.w * 2.0) + 0.5).r;\r\n" \
-	"float light;\r\n" \
-	"if (lightClipPos.z < (lightClipPos.w * 2.0) * (shadowDepth - 0.5))\r\n" \
-	"   light = 1.0;\r\n" \
-	"else {\r\n" \
-	"   vec4 worldPos = modelviewProjInverse * cameraClipPos;\r\n" \
-	"   float lightDist = length (lightPos - worldPos.xyz);\r\n" \
-	"   light = sqrt (min (lightDist, lightRange) / lightRange);\r\n" \
-	"   }\r\n" \
-	"gl_FragColor = vec4 (texture2D (sceneColor, gl_TexCoord [0].xy).rgb * light, 1.0);\r\n" \
-	"}\r\n";
-
-#	else
-
 	"uniform sampler2D sceneColor;\r\n" \
 	"uniform sampler2D sceneDepth;\r\n" \
 	"uniform sampler2DShadow shadowMap;\r\n" \
@@ -300,10 +184,6 @@ const char* shadowMapFS =
 	"   }\r\n" \
 	"gl_FragColor = vec4 (texture2D (sceneColor, gl_TexCoord [0].xy).rgb * light, 1.0);\r\n" \
 	"}\r\n";
-
-#	endif
-
-#endif
 
 //-------------------------------------------------------------------------
 
