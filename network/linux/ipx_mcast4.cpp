414f21f940463ec334e31e29715c1e9699f9c9e3
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-05-04 00:11:52 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@9539 01821946-851b-0410-b7f4-f1477c70e57a

--- network/linux/ipx_mcast4.cpp
+++ network/linux/ipx_mcast4.cpp
@@ -283,7 +283,7 @@ static void ipx_mcast4_InitNetgameAuxData(ipx_socket_t *sk, u_char buf[NETGAME_A
 // The first byte is the version number
 buf[0] = IPX_MCAST4_VERSION;
 // Generate a Random session
-game_addr = inet_makeaddr(239*256 + 255, d_rand() % 0xFFFF);
+game_addr = inet_makeaddr(239*256 + 255, rand() % 0xFFFF);
 memcpy(buf + 1, &game_addr, sizeof(game_addr));
 // Since we're obviously the hosting machine, subscribe to this address
 ipx_mcast4_HandleNetgameAuxData(sk, buf);
