311eae4bf4033e5b6aa15c2999b4dc746a0f8292
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-06-04 09:27:31 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@9967 01821946-851b-0410-b7f4-f1477c70e57a

--- lighting/lightmapshaders.cpp
+++ lighting/lightmapshaders.cpp
@@ -37,7 +37,7 @@ const char *pszLMLightingFS [] = {
 	"uniform float fLightScale;\r\n" \
 	"void main() {\r\n" \
 	"vec4 color = (texture2D (lMapTex, gl_TexCoord [0].xy) + gl_Color) * fLightScale;\r\n" \
-	"gl_FragColor = texture2D (lMapTex, gl_TexCoord [0].xy); //vec4 (matColor.rgb * min (vec3 (1.0, 1.0, 1.0), color.rgb), matColor.a * gl_Color.a);\r\n" \
+	"gl_FragColor = vec4 (matColor.rgb * min (vec3 (1.0, 1.0, 1.0), color.rgb), matColor.a * gl_Color.a);\r\n" \
 	"}"
 	,
 	"uniform sampler2D lMapTex, baseTex;\r\n" \
