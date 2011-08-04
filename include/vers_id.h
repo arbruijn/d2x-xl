144dde4e053d72c067c377c2b4429320857e7d91
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-08-04 22:55:42 +0000

    Fixed: A syntax flaw in a shader program made the shader unusable
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@10188 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.15.248"
+#		define VERSION		"1.15.249"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #	define D2X_MINOR	15
 #endif
 #ifndef D2X_MICRO
-#	define D2X_MICRO	248
+#	define D2X_MICRO	249
 #endif
 
 #define VERSION_TYPE		"Full Version"
