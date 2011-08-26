cbce3c02f1bb7aca6dd71e4320d862991f584926
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-08-26 12:09:20 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@10368 01821946-851b-0410-b7f4-f1477c70e57a

--- menus/monsterballmenu.cpp
+++ menus/monsterballmenu.cpp
@@ -40,6 +40,33 @@
 
 static int nBonusOpt, nSizeModOpt, nPyroForceOpt;
 
+static const char *szWeaponIds [] = {
+	"Laser 1", 
+	"Laser 2", 
+	"Laser 3", 
+	"Laser 4", 
+	"Spreadfire", 
+	"Vulcan", 
+	"Plasma", 
+	"Fusion", 
+	"Superlaser 1", 
+	"Superlaser 2", 
+	"Helix", 
+	"Gauss", 
+	"Phoenix", 
+	"Omega", 
+	"Flare", 
+	"Concussion", 
+	"Homing", 
+	"Smart", 
+	"Mega", 
+	"Flash", 
+	"Guided", 
+	"Mercury", 
+	"Earthshaker", 
+	"Shaker Bomblet"
+	};
+
 static const char *szWeaponTexts [] = {
 	"Laser 1: %1.2f", 
 	"Laser 2: %1.2f", 
@@ -81,43 +108,50 @@ if (nState)
 
 h = sizeofa (szWeaponTexts);
 for (i = j = 0; i <= h; i++, j++, pf++) {
-	m = menu + i;
-	v = m->m_value;
-	if (pf->nForce != nOptionToForce [v]) {
-		pf->nForce = nOptionToForce [v];
-		sprintf (m->m_text, szWeaponTexts [j], float (pf->nForce) / 100.0f);
-		m->m_bRebuild = 1;
+	if ((m = menu [szWeaponIds [i]])) {
+		v = m->Value ();
+		if (pf->nForce != nOptionToForce [v]) {
+			pf->nForce = nOptionToForce [v];
+			sprintf (m->m_text, szWeaponTexts [j], float (pf->nForce) / 100.0f);
+			m->m_bRebuild = 1;
+			}
+		if (pf->nWeaponId == FLARE_ID)
+			i++;
 		}
-	if (pf->nWeaponId == FLARE_ID)
-		i++;
 	}
 
-m = menu + nPyroForceOpt;
-v = m->m_value + 1;
-if (v != extraGameInfo [0].monsterball.forces [24].nForce) {
-	extraGameInfo [0].monsterball.forces [24].nForce = v;
-	sprintf (m->m_text, TXT_MBALL_PYROFORCE, v);
-	m->m_bRebuild = 1;
-	//key = -2;
-	return nCurItem;
+if ((m = menu ["pyro force"])) {
+	v = m->Value () + 1;
+	if (v != extraGameInfo [0].monsterball.forces [24].nForce) {
+		extraGameInfo [0].monsterball.forces [24].nForce = v;
+		sprintf (m->m_text, TXT_MBALL_PYROFORCE, v);
+		m->m_bRebuild = 1;
+		//key = -2;
+		return nCurItem;
+		}
 	}
-m = menu + nBonusOpt;
-v = m->m_value + 1;
-if (v != extraGameInfo [0].monsterball.nBonus) {
-	extraGameInfo [0].monsterball.nBonus = v;
-	sprintf (m->m_text, TXT_GOAL_BONUS, v);
-	m->m_bRebuild = 1;
-	//key = -2;
-	return nCurItem;
+
+if ((m = menu ["bonus"])) {
+	v = m->Value () + 1;
+	if (v != extraGameInfo [0].monsterball.nBonus) {
+		extraGameInfo [0].monsterball.nBonus = v;
+		sprintf (m->m_text, TXT_GOAL_BONUS, v);
+		m->m_bRebuild = 1;
+		//key = -2;
+		return nCurItem;
+		}
 	}
-m = menu + nSizeModOpt;
-v = m->m_value + 2;
-if (v != extraGameInfo [0].monsterball.nSizeMod) {
-	extraGameInfo [0].monsterball.nSizeMod = v;
-	sprintf (m->m_text, TXT_MBALL_SIZE, v / 2, (v & 1) ? 5 : 0);
-	m->m_bRebuild = 1;
-	//key = -2;
+
+if ((m = menu ["size mod"])) {
+	v = m->Value () + 2;
+	if (v != extraGameInfo [0].monsterball.nSizeMod) {
+		extraGameInfo [0].monsterball.nSizeMod = v;
+		sprintf (m->m_text, TXT_MBALL_SIZE, v / 2, (v & 1) ? 5 : 0);
+		m->m_bRebuild = 1;
+		//key = -2;
+		}
 	}
+
 return nCurItem;
 }
 
@@ -165,7 +199,7 @@ void NetworkMonsterballOptions (void)
 	static int choice = 0;
 
 	CMenu					m (35);
-	int					h, i, j, opt = 0, optDefaultForces;
+	int					h, i, j, opt = 0;
 	char					szSlider [60];
 	tMonsterballForce	*pf = extraGameInfo [0].monsterball.forces;
 
@@ -173,50 +207,49 @@ h = (int) sizeofa (optionToWeaponId);
 j = (int) sizeofa (nOptionToForce);
 for (i = opt = 0; i < h; i++, opt++, pf++) {
 	sprintf (szSlider, szWeaponTexts [i], float (pf->nForce) / 100.0f);
-	m.AddSlider (szSlider, ForceToOption (pf->nForce), 0, j - 1, 0, NULL);
+	m.AddSlider (szWeaponIds [i], szSlider, ForceToOption (pf->nForce), 0, j - 1, 0, NULL);
 	if (pf->nWeaponId == FLARE_ID)
-		m.AddText ("", 0);
+		m.AddText ("", "", 0);
 	}
-m.AddText ("", 0);
+m.AddText ("", "", 0);
 sprintf (szSlider + 1, TXT_MBALL_PYROFORCE, pf->nForce);
 *szSlider = *(TXT_MBALL_PYROFORCE - 1);
-nPyroForceOpt = m.AddSlider (szSlider + 1, pf->nForce - 1, 0, 9, 0, NULL);
-m.AddText ("", 0);
+m.AddSlider ("pyro force", szSlider + 1, pf->nForce - 1, 0, 9, 0, NULL);
+m.AddText ("", "", 0);
 sprintf (szSlider + 1, TXT_GOAL_BONUS, extraGameInfo [0].monsterball.nBonus);
 *szSlider = *(TXT_GOAL_BONUS - 1);
-nBonusOpt = m.AddSlider (szSlider + 1, extraGameInfo [0].monsterball.nBonus - 1, 0, 9, 0, HTX_GOAL_BONUS);
+m.AddSlider ("bonus", szSlider + 1, extraGameInfo [0].monsterball.nBonus - 1, 0, 9, 0, HTX_GOAL_BONUS);
 i = extraGameInfo [0].monsterball.nSizeMod;
 sprintf (szSlider + 1, TXT_MBALL_SIZE, i / 2, (i & 1) ? 5 : 0);
 *szSlider = *(TXT_MBALL_SIZE - 1);
-nSizeModOpt = m.AddSlider (szSlider + 1, extraGameInfo [0].monsterball.nSizeMod - 2, 0, 8, 0, HTX_MBALL_SIZE);
+m.AddSlider ("size mod", szSlider + 1, extraGameInfo [0].monsterball.nSizeMod - 2, 0, 8, 0, HTX_MBALL_SIZE);
 m.AddText ("", 0);
-optDefaultForces = m.AddMenu ("Set default values", 0, NULL);
+m.AddMenu ("default values", "Set default values", 0, NULL);
 
 for (;;) {
 	i = m.Menu (NULL, "Monsterball Impact Forces", MonsterballMenuCallback, &choice);
 	if (i == -1)
 		break;
-	if (i != optDefaultForces)
+	if (i != m.IndexOf ("default values"))
 		break;
 	InitMonsterballSettings (&extraGameInfo [0].monsterball);
 	pf = extraGameInfo [0].monsterball.forces;
 	for (i = 0; i <= h; i++, pf++) {
-		m [i].m_value = ForceToOption (pf->nForce);
+		m [szWeaponIds [i]]->Value () = ForceToOption (pf->nForce);
 		if (pf->nWeaponId == FLARE_ID)
 			i++;
 		}
-	m [nPyroForceOpt].m_value = pf->nForce - 1;
-	m [nSizeModOpt].m_value = extraGameInfo [0].monsterball.nSizeMod - 2;
+	m ["pyro force"]->Value () = pf->nForce - 1;
+	m ["size mod"]->Value () = extraGameInfo [0].monsterball.nSizeMod - 2;
 	}
 #if 0
 pf = extraGameInfo [0].monsterball.forces;
 for (i = 0; i < h; i++, pf++)
-	pf->nForce = nOptionToForce [mat [i].m_value];
+	pf->nForce = nOptionToForce [m [i].Value ()];
 #endif
-pf->nForce = m [nPyroForceOpt].m_value + 1;
-extraGameInfo [0].monsterball.nSizeMod = m [nSizeModOpt].m_value + 2;
-extraGameInfo [0].monsterball.nBonus = m [nBonusOpt].m_value + 1;
+pf->nForce = m ["pyro force"]->Value () + 1;
+extraGameInfo [0].monsterball.nSizeMod = m ["size mod"]->Value () + 2;
+extraGameInfo [0].monsterball.nBonus = m ["bonus"]->Value () + 1;
 }
 
 //------------------------------------------------------------------------------
-
