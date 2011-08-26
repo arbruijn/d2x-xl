4ecb81574fd6c8460924ffa53a1d6f0357d3889d
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2011-08-26 13:16:20 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@10377 01821946-851b-0410-b7f4-f1477c70e57a

--- menus/soundmenu.cpp
+++ menus/soundmenu.cpp
@@ -95,102 +95,122 @@ int SoundMenuCallback (CMenu& menu, int& nKey, int nCurItem, int nState)
 if (nState)
 	return nCurItem;
 
-if (soundOpts.nChannels >= 0) {
-	if (gameStates.sound.nSoundChannels != menu [soundOpts.nChannels].m_value + 2) {
-		gameStates.sound.nSoundChannels = menu [soundOpts.nChannels].m_value + 2;
-		sprintf (menu [soundOpts.nChannels].m_text, TXT_SOUND_CHANNEL_COUNT, pszLowMediumHigh [gameStates.sound.nSoundChannels - 2]);
-		menu [soundOpts.nChannels].m_bRebuild = 1;
+CMenuItem*	m;
+int			v;
+
+if ((m = menu ["channels"])) {
+	v = m->Value ();
+	if (gameStates.sound.nSoundChannels != v + 2) {
+		gameStates.sound.nSoundChannels = v + 2;
+		sprintf (m->Text (), TXT_SOUND_CHANNEL_COUNT, pszLowMediumHigh [gameStates.sound.nSoundChannels - 2]);
+		m->Rebuild ();
 		}
 	}
 
-if (soundOpts.nGatling >= 0) {
-	if (gameOpts->sound.bGatling != menu [soundOpts.nGatling].m_value) {
-		gameOpts->sound.bGatling = menu [soundOpts.nGatling].m_value;
+if ((m = menu ["gatling"])) {
+	v = m->Value ();
+	if (gameOpts->sound.bGatling != v) {
+		gameOpts->sound.bGatling = v;
 		nKey = -2;
 		}
 	}
 
-if (gameConfig.nAudioVolume [0] != menu [soundOpts.nDigiVol].m_value) {
-	gameConfig.nAudioVolume [0] = menu [soundOpts.nDigiVol].m_value;
-	if (gameOpts->sound.bLinkVolumes) {
-		gameConfig.nAudioVolume [1] = gameConfig.nAudioVolume [0];
-		audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32767) / 8, 1);
-		}
-	audio.SetFxVolume ((gameConfig.nAudioVolume [0] * 32767) / 8);
-	audio.PlaySound (SOUND_DROP_BOMB);
-	}
-
-if (soundOpts.nAmbientVol >= 0) {
-	if (gameConfig.nAudioVolume [1] != menu [soundOpts.nAmbientVol].m_value) {
-		gameConfig.nAudioVolume [1] = menu [soundOpts.nAmbientVol].m_value;
-		audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32767) / 8, 1);
+if ((m = menu ["fx volume"])) {
+	v = m->Value ();
+	if (gameConfig.nAudioVolume [0] != v) {
+		gameConfig.nAudioVolume [0] = v;
+		if (gameOpts->sound.bLinkVolumes) {
+			gameConfig.nAudioVolume [1] = gameConfig.nAudioVolume [0];
+			audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32767) / 8, 1);
+			}
+		audio.SetFxVolume ((gameConfig.nAudioVolume [0] * 32767) / 8);
+		audio.PlaySound (SOUND_DROP_BOMB);
 		}
 	}
 
-if (gameOpts->sound.bLinkVolumes != menu [soundOpts.nLinkVols].m_value) {
-	if ((gameOpts->sound.bLinkVolumes = menu [soundOpts.nLinkVols].m_value)) {
-		gameConfig.nAudioVolume [1] = gameConfig.nAudioVolume [0];
+if ((m = menu ["ambient volume"])) {
+	v = m->Value ();
+	if (gameConfig.nAudioVolume [1] != v) {
+		gameConfig.nAudioVolume [1] = v;
 		audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32767) / 8, 1);
 		}
-	nKey = -2;
 	}
 
-if (menu [soundOpts.nRedbook].m_value != redbook.Enabled ()) {
-	if (menu [soundOpts.nRedbook].m_value && !gameOpts->sound.bUseRedbook) {
-		MsgBox (TXT_SORRY, NULL, 1, TXT_OK, TXT_REDBOOK_DISABLED);
-		menu [soundOpts.nRedbook].m_value = 0;
-		menu [soundOpts.nRedbook].m_bRebuild = 1;
+if ((m = menu ["link volumes"])) {
+	v = m->Value ();
+	if (gameOpts->sound.bLinkVolumes != v) {
+		if (gameOpts->sound.bLinkVolumes = v) {
+			gameConfig.nAudioVolume [1] = gameConfig.nAudioVolume [0];
+			audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32767) / 8, 1);
+			}
+		nKey = -2;
 		}
-	else {
-		redbook.Enable (menu [soundOpts.nRedbook].m_value);
-		if (redbook.Enabled ()) {
-			if (gameStates.app.nFunctionMode == FMODE_MENU)
-				songManager.Play (SONG_TITLE, 1);
-			else if (gameStates.app.nFunctionMode == FMODE_GAME)
-				songManager.PlayLevelSong (missionManager.nCurrentLevel, gameStates.app.bGameRunning);
-			else
-				Int3 ();
+	}
 
-			if (menu [soundOpts.nRedbook].m_value && !redbook.Playing ()) {
-				redbook.Enable (0);
-				gameStates.menus.nInMenu = 0;
-				MsgBox (TXT_SORRY, NULL, 1, TXT_OK, TXT_MUSIC_NOCD);
-				gameStates.menus.nInMenu = 1;
-				menu [soundOpts.nRedbook].m_value = 0;
-				menu [soundOpts.nRedbook].m_bRebuild = 1;
+if ((m = menu ["redbook volume"])) {
+	v = m->Value ();
+	if (redbook.Enabled () != v) {
+		if (v && !gameOpts->sound.bUseRedbook) {
+			MsgBox (TXT_SORRY, NULL, 1, TXT_OK, TXT_REDBOOK_DISABLED);
+			menu [soundOpts.nRedbook].Value () = 0;
+			menu [soundOpts.nRedbook].Rebuild ();
+			}
+		else {
+			redbook.Enable (v);
+			if (redbook.Enabled ()) {
+				if (gameStates.app.nFunctionMode == FMODE_MENU)
+					songManager.Play (SONG_TITLE, 1);
+				else if (gameStates.app.nFunctionMode == FMODE_GAME)
+					songManager.PlayLevelSong (missionManager.nCurrentLevel, gameStates.app.bGameRunning);
+				else
+					Int3 ();
+
+				if (v && !redbook.Playing ()) {
+					redbook.Enable (0);
+					gameStates.menus.nInMenu = 0;
+					MsgBox (TXT_SORRY, NULL, 1, TXT_OK, TXT_MUSIC_NOCD);
+					gameStates.menus.nInMenu = 1;
+					m->Value () = 0;
+					m->Rebuild ();
+					}
 				}
 			}
+		if ((m = menu ["music volume"])) {
+			m->SetText (redbook.Enabled () ? const_cast<char*> (TXT_CD_VOLUME) : const_cast<char*> (TXT_MIDI_VOLUME));
+			m->Value () = redbook.Enabled () ? gameConfig.nRedbookVolume : gameConfig.nMidiVolume;
+			m->Rebuild ();
+			}
 		}
-	menu [soundOpts.nMusicVol].SetText (redbook.Enabled () ? const_cast<char*> (TXT_CD_VOLUME) : const_cast<char*> (TXT_MIDI_VOLUME));
-	menu [soundOpts.nMusicVol].m_value = redbook.Enabled () ? gameConfig.nRedbookVolume : gameConfig.nMidiVolume;
-	menu [soundOpts.nMusicVol].m_bRebuild = 1;
-	}
+	}	
 
-if (redbook.Enabled ()) {
-	if (gameConfig.nRedbookVolume != menu [soundOpts.nMusicVol].m_value)   {
-		gameConfig.nRedbookVolume = menu [soundOpts.nMusicVol].m_value;
-		redbook.SetVolume (gameConfig.nRedbookVolume);
+if ((m = menu ["music volume"])) {
+	v = m->Value ();
+	if (redbook.Enabled ()) {
+		if (gameConfig.nRedbookVolume != v) {
+			gameConfig.nRedbookVolume = v;
+			redbook.SetVolume (gameConfig.nRedbookVolume);
+			}
 		}
-	}
-else {
-	if (gameConfig.nMidiVolume != menu [soundOpts.nMusicVol].m_value) {
-		int bSongPlaying = (gameConfig.nMidiVolume > 0);
+	else {
+		if (gameConfig.nMidiVolume != v) {
+			int bSongPlaying = (gameConfig.nMidiVolume > 0);
 
-		if (gameConfig.nMidiVolume * menu [soundOpts.nMusicVol].m_value == 0) //=> midi gets either turned on or off
-			nKey = -2;
- 		gameConfig.nMidiVolume = menu [soundOpts.nMusicVol].m_value;
-		if (gameConfig.nMidiVolume < 1) {
-			midi.PlaySong (NULL, NULL, NULL, 1, 0);	// fade out first
-			midi.SetVolume (128 * gameConfig.nMidiVolume / 8);
-			}	
-		else {
-			midi.SetVolume (128 * gameConfig.nMidiVolume / 8);
-			if (!bSongPlaying) {
-			//audio.StopAllChannels ();
-				if (gameStates.app.bGameRunning)
-					songManager.PlayLevelSong (missionManager.nCurrentLevel ? missionManager.nCurrentLevel : 1, 1);
-				else
-					songManager.Play (SONG_TITLE, 1);
+			if (gameConfig.nMidiVolume * v == 0) //=> midi gets either turned on or off
+				nKey = -2;
+			gameConfig.nMidiVolume = v;
+			if (gameConfig.nMidiVolume < 1) {
+				midi.PlaySong (NULL, NULL, NULL, 1, 0);	// fade out first
+				midi.SetVolume (128 * gameConfig.nMidiVolume / 8);
+				}	
+			else {
+				midi.SetVolume (128 * gameConfig.nMidiVolume / 8);
+				if (!bSongPlaying) {
+				//audio.StopAllChannels ();
+					if (gameStates.app.bGameRunning)
+						songManager.PlayLevelSong (missionManager.nCurrentLevel ? missionManager.nCurrentLevel : 1, 1);
+					else
+						songManager.Play (SONG_TITLE, 1);
+					}
 				}
 			}
 		}
@@ -226,7 +246,6 @@ void SoundMenu (void)
 	char	szVolume [50];
 #endif
 	int	i;
-	int	optReverse, optShipSound = -1, optMissileSound = -1, optSpeedUpSound = -1, optFadeMusic = -1, optShieldWarn = -1;
 	int	bSongPlaying = (gameConfig.nMidiVolume > 0);
 
 InitStrings ();
@@ -239,56 +258,43 @@ do {
 	if (gameOpts->app.bNotebookFriendly || gameOpts->app.bExpertMode) {
 		sprintf (szSlider + 1, TXT_SOUND_CHANNEL_COUNT, pszLowMediumHigh [gameStates.sound.nSoundChannels - 2]);
 		*szSlider = *(TXT_SOUND_CHANNEL_COUNT - 1);
-		soundOpts.nChannels = m.AddSlider (szSlider + 1, gameStates.sound.nSoundChannels - 2, 0, 2, KEY_C, HTX_SOUND_CHANNEL_COUNT);  
+		m.AddSlider ("channels", szSlider + 1, gameStates.sound.nSoundChannels - 2, 0, 2, KEY_C, HTX_SOUND_CHANNEL_COUNT);  
 		}
-	else
-		soundOpts.nChannels = -1;
-	soundOpts.nDigiVol = m.AddSlider (TXT_FX_VOLUME, gameConfig.nAudioVolume [0], 0, 8, KEY_F, HTX_ONLINE_MANUAL);
-	if (gameOpts->sound.bLinkVolumes)
-		soundOpts.nAmbientVol = -1;
-	else
-		soundOpts.nAmbientVol = m.AddSlider (TXT_AMBIENT_VOLUME, gameConfig.nAudioVolume [1], 0, 8, KEY_A, HTX_ONLINE_MANUAL);
-	soundOpts.nMusicVol = m.AddSlider (redbook.Enabled () ? TXT_CD_VOLUME : TXT_MIDI_VOLUME, 
-												  redbook.Enabled () ? gameConfig.nRedbookVolume : gameConfig.nMidiVolume, 
-												  0, 8, KEY_M, HTX_ONLINE_MANUAL);
-	m.AddText ("", 0);
-	soundOpts.nLinkVols = m.AddCheck (TXT_LINK_AUDIO_VOLUMES, gameOpts->sound.bLinkVolumes, KEY_L, HTX_ONLINE_MANUAL);
-	soundOpts.nRedbook = m.AddCheck (TXT_REDBOOK_ENABLED, redbook.Enabled (), KEY_C, HTX_ONLINE_MANUAL);
-	optReverse = m.AddCheck (TXT_REVERSE_STEREO, gameConfig.bReverseChannels, KEY_R, HTX_ONLINE_MANUAL);
+	m.AddSlider ("fx volume", TXT_FX_VOLUME, gameConfig.nAudioVolume [0], 0, 8, KEY_F, HTX_ONLINE_MANUAL);
+	if (!gameOpts->sound.bLinkVolumes)
+		m.AddSlider ("ambient volume", TXT_AMBIENT_VOLUME, gameConfig.nAudioVolume [1], 0, 8, KEY_A, HTX_ONLINE_MANUAL);
+	m.AddSlider ("music volume", redbook.Enabled () ? TXT_CD_VOLUME : TXT_MIDI_VOLUME, 
+					 redbook.Enabled () ? gameConfig.nRedbookVolume : gameConfig.nMidiVolume, 
+					 0, 8, KEY_M, HTX_ONLINE_MANUAL);
+	m.AddText ("", "", 0);
+	m.AddCheck ("link volumes", TXT_LINK_AUDIO_VOLUMES, gameOpts->sound.bLinkVolumes, KEY_L, HTX_ONLINE_MANUAL);
+	m.AddCheck ("redbook", TXT_REDBOOK_ENABLED, redbook.Enabled (), KEY_C, HTX_ONLINE_MANUAL);
+	m.AddCheck ("reverse stereo", TXT_REVERSE_STEREO, gameConfig.bReverseChannels, KEY_R, HTX_ONLINE_MANUAL);
 #if 1
-	if (redbook.Enabled () || !gameConfig.nMidiVolume)
-		optFadeMusic = -1;
-	else
-		optFadeMusic = m.AddCheck (TXT_FADE_MUSIC, gameOpts->sound.bFadeMusic, KEY_F, HTX_FADE_MUSIC);
+	if (!redbook.Enabled () && gameConfig.nMidiVolume)
+		m.AddCheck ("fade music", TXT_FADE_MUSIC, gameOpts->sound.bFadeMusic, KEY_F, HTX_FADE_MUSIC);
 #endif
-	if (gameStates.app.bNostalgia) 
-		optShipSound = 
-		optMissileSound =
-		optSpeedUpSound =
-		soundOpts.nGatling = -1;
-	else {
-		m.AddText ("", 0);
-		optShipSound = m.AddCheck (TXT_SHIP_SOUND, gameOpts->sound.bShip, KEY_S, HTX_SHIP_SOUND);
-		optMissileSound = m.AddCheck (TXT_MISSILE_SOUND, gameOpts->sound.bMissiles, KEY_M, HTX_MISSILE_SOUND);
-		soundOpts.nGatling = m.AddCheck (TXT_GATLING_SOUND, gameOpts->sound.bGatling, KEY_G, HTX_GATLING_SOUND);
+	if (!gameStates.app.bNostalgia) {
+		m.AddText ("", "", 0);
+		m.AddCheck ("ship sound", TXT_SHIP_SOUND, gameOpts->sound.bShip, KEY_S, HTX_SHIP_SOUND);
+		m.AddCheck ("missile sound", TXT_MISSILE_SOUND, gameOpts->sound.bMissiles, KEY_M, HTX_MISSILE_SOUND);
+		m.AddCheck ("gatling sound", TXT_GATLING_SOUND, gameOpts->sound.bGatling, KEY_G, HTX_GATLING_SOUND);
 		if (gameOpts->sound.bGatling)
-			optSpeedUpSound = m.AddCheck (TXT_SPINUP_SOUND, extraGameInfo [0].bGatlingSpeedUp, KEY_U, HTX_SPINUP_SOUND);
-		if (gameOpts->render.cockpit.bTextGauges)
-			optShieldWarn = -1;
-		else
-			optShieldWarn = m.AddCheck (TXT_SHIELD_WARNING, gameOpts->gameplay.bShieldWarning, KEY_W, HTX_CPIT_SHIELDWARN);
+			m.AddCheck ("spinup sound", TXT_SPINUP_SOUND, extraGameInfo [0].bGatlingSpeedUp, KEY_U, HTX_SPINUP_SOUND);
+		if (!gameOpts->render.cockpit.bTextGauges)
+			m.AddCheck ("shield warning", TXT_SHIELD_WARNING, gameOpts->gameplay.bShieldWarning, KEY_W, HTX_CPIT_SHIELDWARN);
 		}
 
 	i = m.Menu (NULL, TXT_SOUND_OPTS, SoundMenuCallback, &choice);
-	redbook.Enable (m [soundOpts.nRedbook].m_value);
-	gameConfig.bReverseChannels = m [optReverse].m_value;
+	redbook.Enable (m ["redbook"]->Value ());
+	gameConfig.bReverseChannels = m ["reverse stereo"]->Value ();
 	if (!gameStates.app.bNostalgia) {
-		GET_VAL (gameOpts->sound.bFadeMusic, optFadeMusic);
-		GET_VAL (gameOpts->sound.bShip, optShipSound);
-		GET_VAL (gameOpts->sound.bMissiles, optMissileSound);
-		GET_VAL (gameOpts->sound.bGatling, soundOpts.nGatling);
-		GET_VAL (extraGameInfo [0].bGatlingSpeedUp, optSpeedUpSound);
-		GET_VAL (gameOpts->gameplay.bShieldWarning, optShieldWarn);
+		GET_VAL (gameOpts->sound.bFadeMusic, "fade music");
+		GET_VAL (gameOpts->sound.bShip, "ship sound");
+		GET_VAL (gameOpts->sound.bMissiles, "missile sound");
+		GET_VAL (gameOpts->sound.bGatling, "gatling sound");
+		GET_VAL (extraGameInfo [0].bGatlingSpeedUp, "spinup sound");
+		GET_VAL (gameOpts->gameplay.bShieldWarning, "shield warning");
 		if (gameStates.app.bGameRunning && !(gameOpts->sound.bShip && gameOpts->sound.bGatling))
 			audio.DestroyObjectSound (LOCALPLAYER.nObject);
 		}
