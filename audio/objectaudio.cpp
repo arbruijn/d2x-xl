236efa3a38f25bb62cb3e05596cab16cf0395633
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-02-24 01:09:57 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@10747 01821946-851b-0410-b7f4-f1477c70e57a

--- audio/objectaudio.cpp
+++ audio/objectaudio.cpp
@@ -1039,7 +1039,7 @@ for (segP = SEGMENTS.Buffer (), nSegment = 0; nSegment <= gameData.segs.nLastSeg
 		if (0 > (nSideSounds [nSide] = SideIsSoundSource (nSegment, nSide)))
 			nSegSoundSources++;
 	for (nSide = 0, nSegSoundSources = 0; nSide < MAX_SIDES_PER_SEGMENT; nSide++) 
-		if (nSideSounds [nSide])
+		if (nSideSounds [nSide] >= 0)
 			audio.CreateSegmentSound (nSideSounds [nSide], nSegment, nSide, segP->SideCenter (nSide), 1, I2X (1) / (2 * nSegSoundSources));
 	}
 
