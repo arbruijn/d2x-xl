cea295705f546f6144d1c572e90c1055f1a5e094
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-03-13 11:15:33 +0000

    Added VS 2010 stuff for D2X-XL.
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@10886 01821946-851b-0410-b7f4-f1477c70e57a

---
+++ VC10/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by d2x.w32.rc
+//
+#define IDI_D2XW32                      101
+#define IDI_DESCENT2                    102
+#define IDI_D2X_XL                      103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
