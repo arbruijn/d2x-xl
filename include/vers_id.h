cc11261ae8749672a249830ae7ad024bd12f63db
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-03-13 01:14:29 +0000

    Speeded up transparency rendering by about 10% on multi core cpus
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@10883 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.15.317"
+#		define VERSION		"1.15.318"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #	define D2X_MINOR	15
 #endif
 #ifndef D2X_MICRO
-#	define D2X_MICRO	317
+#	define D2X_MICRO	318
 #endif
 
 #define VERSION_TYPE		"Full Version"
