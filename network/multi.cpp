3946cad0ca768017288387b070e21767fe50a8c1
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-03-22 01:10:57 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@11029 01821946-851b-0410-b7f4-f1477c70e57a

--- network/multi.cpp
+++ network/multi.cpp
@@ -5119,10 +5119,12 @@ for (i = 0; i < MAX_POWERUP_TYPES; i++) {
 #if DBG
 	PowerupsInMine (i);
 #endif
-	if (MultiPowerupIs4Pack (i + 1) && MissingPowerups (i + 1)) {
-		for (j = h / 4; j; j--)
-			MaybeDropNetPowerup (-1, i + 1, FORCE_DROP);
-		h %= 4;
+	if (MultiPowerupIs4Pack (i + 1)) {
+		if (MissingPowerups (i + 1)) {
+			for (j = h / 4; j; j--)
+				MaybeDropNetPowerup (-1, i + 1, FORCE_DROP);
+			h %= 4;
+			}
 		i++;
 		}
 	else 
