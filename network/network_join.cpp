5c9f1d1eb42169c4d6199723d99d675eb403bd3f
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-03-24 11:38:30 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@11101 01821946-851b-0410-b7f4-f1477c70e57a

--- network/network_join.cpp
+++ network/network_join.cpp
@@ -294,7 +294,7 @@ if ((gameData.multiplayer.nPlayers < gameData.multiplayer.nMaxPlayers) &&
 int oldestPlayer = -1;
 fix oldestTime = TimerGetApproxSeconds ();
 for (int i = 0; i < gameData.multiplayer.nPlayers; i++) {
-	if (!gameData.multiplayer.players [i].connected && (networkData.nLastPacketTime [i] < oldestTime)) {
+	if (!gameData.multiplayer.players [i].Connected () && (networkData.nLastPacketTime [i] < oldestTime)) {
 		oldestTime = networkData.nLastPacketTime [i];
 		oldestPlayer = i;
 		}
