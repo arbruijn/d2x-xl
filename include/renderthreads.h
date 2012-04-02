8874b52b3af7ef229ac6dfbeb0d25af67b3cd796
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-04-02 09:28:56 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@11237 01821946-851b-0410-b7f4-f1477c70e57a

--- include/renderthreads.h
+++ include/renderthreads.h
@@ -41,7 +41,7 @@ void CreateRenderThreads (void);
 void DestroyRenderThreads (void);
 bool WaitForRenderThreads (void);
 void CreateEffectsThread (void);
-void StartEffectsThread (void);
+void StartEffectsThread (int nWindow);
 void DestroyEffectsThread (void);
 void ControlEffectsThread (void);
 bool WaitForEffectsThread (void);
