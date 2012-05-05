7af37a1ac8553c3e58f6cbb3ba91ad79ef386789
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2012-05-05 17:08:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@11453 01821946-851b-0410-b7f4-f1477c70e57a

--- include/rendermodel.h
+++ include/rendermodel.h
@@ -1,22 +1,3 @@
-/*
-THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
-SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
-END-USERS, AND SUBJECT TO ALL OF THE TERMS AND CONDITIONS HEREIN, GRANTS A
-ROYALTY-FREE, PERPETUAL LICENSE TO SUCH END-USERS FOR USE BY SUCH END-USERS
-IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
-SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
-FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
-CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
-AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
-COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
-*/
-
-/*
- *
- * Header file for Inferno.  Should be included in all source files.
- *
- */
-
 #ifndef _RENDERMODEL_H
 #define _RENDERMODEL_H
 
@@ -40,17 +21,17 @@ class CRenderVertex {
 	public:
 		CFloatVector3			m_vertex;
 		CFloatVector3			m_normal;
-		CFloatVector				m_color;
+		CFloatVector			m_color;
 		tTexCoord2f				m_texCoord;
 		};
 
 class CVertex {
 	public:
 		tTexCoord2f				m_texCoord;
-		CFloatVector				m_renderColor;
+		CFloatVector			m_renderColor;
 		CFloatVector3			m_vertex;
 		CFloatVector3			m_normal;
-		CFloatVector				m_baseColor;
+		CFloatVector			m_baseColor;
 		ushort					m_nIndex;
 		char						m_bTextured;
 	};
