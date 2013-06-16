1b3a2f6f4cec24c4434858a9de25a55224392c48
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2013-06-16 13:12:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@12212 01821946-851b-0410-b7f4-f1477c70e57a

--- include/menu.h
+++ include/menu.h
@@ -58,24 +58,24 @@ extern char DOWN_ARROW_MARKER[2];
 
 #define EXPMODE_DEFAULTS 0
 
-#define NMCLAMP (_v,_min,_max)	 ( (_v) < (_min) ? (_min) : (_v) > (_max) ? (_max) : (_v))
-#define NMBOOL (_v) ( (_v) != 0)
+#define NMCLAMP(_v,_min,_max)	 ((_v) < (_min) ? (_min) : (_v) > (_max) ? (_max) : (_v))
+#define NMBOOL(_v) ((_v) != 0)
 
-#define GET_VAL (_v,_id)	if (m.Available (_id)) (_v) = m.Value (_id)
+#define GET_VAL(_v,_id)	if (m.Available (_id)) (_v) = m.Value (_id)
 
-#define MENU_KEY (_k,_d)	 ( (_k) < 0) ? (_d) : ( (_k) == 0) ? 0 : gameStates.app.bEnglish ? toupper (KeyToASCII (_k)) : (_k)
+#define MENU_KEY(_k,_d)	 ((_k) < 0) ? (_d) : ((_k) == 0) ? 0 : gameStates.app.bEnglish ? toupper (KeyToASCII (_k)) : (_k)
 
 #define PROGRESS_INCR		20
-#define PROGRESS_STEPS (_i)	 ( ( (_i) + PROGRESS_INCR - 1) / PROGRESS_INCR)
+#define PROGRESS_STEPS(_i)	 (( (_i) + PROGRESS_INCR - 1) / PROGRESS_INCR)
 
 //------------------------------------------------------------------------------
 
-#define INIT_PROGRESS_LOOP (_i,_j,_max) \
-	if ( (_i) < 0) { \
+#define INIT_PROGRESS_LOOP(_i,_j,_max) \
+	if ((_i) < 0) { \
 		 (_i) = 0; (_j) = (_max);} \
 	else { \
 		 (_j) = (_i) + PROGRESS_INCR; \
-		if ( (_j) > (_max)) \
+		if ((_j) > (_max)) \
 			 (_j) = (_max); \
 		}
 
