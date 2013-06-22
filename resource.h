9423f2f6458cc672c49a2d762c9ee37526748c7d
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2013-06-22 12:39:08 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@12251 01821946-851b-0410-b7f4-f1477c70e57a

---
+++ resource.h
@@ -0,0 +1,16 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by d2x-xl.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        203
+#define _APS_NEXT_COMMAND_VALUE         32956
+#define _APS_NEXT_CONTROL_VALUE         1711
+#define _APS_NEXT_SYMED_VALUE           105
+#endif
+#endif
