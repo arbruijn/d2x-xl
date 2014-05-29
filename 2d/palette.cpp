853d7508022bfb58062a10c235d841574c367bd4
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2014-05-29 17:42:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@12671 01821946-851b-0410-b7f4-f1477c70e57a

--- 2d/palette.cpp
+++ 2d/palette.cpp
@@ -368,7 +368,7 @@ BumpEffect (float (red) / 64.0f, float (green) / 64.0f, float (blue) / 64.0f);
 void CPaletteManager::BumpEffect (float red, float green, float blue)
 {
 //	float	maxVal = (paletteManager.FadeDelay () ? 60 : MAX_PALETTE_ADD) / 64.0f;
-float fScale = FadeScale ();
+float fScale = Clamp (FadeScale (), 0.0f, 1.0f);
 SetEffect (m_data.effect.Red () * fScale + red, m_data.effect.Green () * fScale + green, m_data.effect.Blue () * fScale + blue);
 }
 
