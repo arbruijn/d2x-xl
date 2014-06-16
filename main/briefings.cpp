03f151ede6c85ae565793b2cc968848f5ef491a7
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2014-06-16 14:01:58 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@12889 01821946-851b-0410-b7f4-f1477c70e57a

--- main/briefings.cpp
+++ main/briefings.cpp
@@ -880,6 +880,7 @@ int CBriefing::InKey (void)
 	int	funcRes = KeyInKey ();
 
 if (funcRes == KEY_PRINT_SCREEN) {
+	gameStates.app.bSaveScreenShot = 1;
 	SaveScreenShot (NULL, 0);
 	return 0;				//say no key pressed
 	}
@@ -1327,10 +1328,6 @@ if (!m_info.bRedraw && m_info.nDelayCount) {
 		return 1;
 	if (keypress == KEY_ESC)
 		return 0;
-	if (keypress == KEY_PRINT_SCREEN) {
-		gameStates.app.bSaveScreenShot = 1;
-		return 1;
-		}
 	if ((keypress == KEY_SPACEBAR) || (keypress == KEY_ENTER)) {
 		StopSound (m_info.nBotChannel);
 		m_info.nDelayCount = 0;
