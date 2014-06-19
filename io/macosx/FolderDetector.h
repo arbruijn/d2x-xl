73b3278c8522e472959a5116b0aa5c93b654deba
itaylo <itaylo@01821946-851b-0410-b7f4-f1477c70e57a>
2014-06-19 17:18:03 +0000

    Remove misc Mac items. Move to new OS X build tree
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@12949 01821946-851b-0410-b7f4-f1477c70e57a

--- io/macosx/FolderDetector.h
+++
@@ -1,9 +0,0 @@
-//
-//  FolderDetector.h
-//  d2x-xl
-//
-//  Created by Simone Manganelli on 2006-01-09.
-//
-
-void GetOSXAppFolder(char* szDataRootDir, char* szGameDir); 
-char *GetMacOSXCacheFolder();
--- io/macosx/MacErrorMsg.h
+++
@@ -1,8 +0,0 @@
-//
-//  MacErrorMsg.h
-//  d2x-xl
-//
-//  Created by Simone Manganelli on 2006-08-02.
-//
-
-void NativeMacOSXMessageBox(const char *pszMsg);
