92d8f912451df2623904f30ab7152f0d3fcf2855
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2014-06-24 20:49:12 +0000

    When only looking for a folder, the Linux version of FileFindFirst wouldn't detect a folder with no subfolders in it
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@13011 01821946-851b-0410-b7f4-f1477c70e57a

--- io/linux/findfile.cpp
+++ io/linux/findfile.cpp
@@ -54,7 +54,7 @@ if (!pszFilter)
 	return -1;
 //PrintLog (1, "FileFindFirst (%s)\n", pszFilter);
 memset (ffsP, 0, sizeof (*ffsP));
-#if 0
+#if 1
 if (*pszFilter && (*pszFilter != '.') && (*pszFilter != '/'))
 	strcpy (ffsP->szDir, gameFolders.game.szRoot); // if only a file spec given, put standard game folder in front of it
 else
@@ -67,6 +67,13 @@ for (i = (int) strlen (ffsP->szDir), j = (int) strlen (pszFilter); i; j--)
 	if (ffsP->szDir [--i] == '/') 
 		break;
 ffsP->szDir [i] = '\0';
+
+*ffsP->name = '\0';
+//PrintLog (0, "opendir (%s)\n", ffsP->szDir);
+if (!(ffsP->dirP = opendir (ffsP->szDir)))
+	return -1;
+if (!*pszFilter) // only looking for a folder
+	return 0;
 //PrintLog (0, "FileFindFirst (%s, %s)\n", ffsP->szDir, pszFilter);
 // create regular search expression, "*" -> ".*", "." -> "\."
 for (i = 0; pszFilter [j]; i++, j++) {
@@ -83,15 +90,13 @@ for (i = 0; pszFilter [j]; i++, j++) {
 	}
 szFilter [i] = '\0';
 //PrintLog (0, "regcomp (%s)\n", szFilter);
+// compile the regular expression szFilter and place the result in ffsP->filter
 if ((i = regcomp (&ffsP->filter, szFilter, flags))) {
 	char szError [200];
 	regerror (i, &ffsP->filter, szError, 200);
 	//PrintLog (-1, "regcomp : error %s\n", szError);
 	return -1;
 	}
-//PrintLog (0, "opendir (%s)\n", ffsP->szDir);
-if (!(ffsP->dirP = opendir (ffsP->szDir)))
-	return -1;
 return FileFindNext (ffsP, nFlags);
 }
 
