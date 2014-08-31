5413fd9b5dc1d4d3692cc6995fe7f6b66a40ce85
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2014-08-31 18:14:59 +0000

    Bumped the program version number
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@13599 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.17.79"
+#		define VERSION		"1.17.80"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #	define D2X_MINOR	17
 #endif
 #ifndef D2X_MICRO
-#	define D2X_MICRO	79
+#	define D2X_MICRO	80
 #endif
 
 #define VERSION_TYPE		"Full Version"
