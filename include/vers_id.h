14deabe7d1628a0297e9fad684e465be1a0407dd
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2015-04-14 22:12:39 +0000

    Fixed: The diagnosis function could erroneously separate connected triangular segment faces Fixed: The diagnosis function did not print diagnostic messages regarding flawed collapsed segment faces
    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@13833 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.17.120"
+#		define VERSION		"1.17.121"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #	define D2X_MINOR	17
 #endif
 #ifndef D2X_MICRO
-#	define D2X_MICRO	120
+#	define D2X_MICRO	121
 #endif
 
 #define VERSION_TYPE		"Full Version"
