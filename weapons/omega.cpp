210ecd18a6eab1adc43476947ec5efab7784c3d0
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2015-04-29 22:52:32 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@13914 01821946-851b-0410-b7f4-f1477c70e57a

--- weapons/omega.cpp
+++ weapons/omega.cpp
@@ -264,7 +264,7 @@ if (SlowMotionActive ()) {
 	nTargetObj = -1;
 else
 #endif
-	nTargetObj = weaponObjP->FindVisibleHomingTarget (*vMuzzle);
+	nTargetObj = weaponObjP->FindVisibleHomingTarget (*vMuzzle, 0);
 nParentSeg = bSpectate ? gameStates.app.nPlayerSegment : parentObjP->info.nSegment;
 
 if (0 > (nFiringSeg = FindSegByPos (*vMuzzle, nParentSeg, 1, 0))) {
