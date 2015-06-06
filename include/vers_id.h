74d8e7edb0a82cfd851d7c5f145335556bba69fc
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2015-06-06 23:15:35 +0000

    git-svn-id: svn://svn.code.sf.net/p/d2x-xl/code/trunk@14509 01821946-851b-0410-b7f4-f1477c70e57a

--- include/vers_id.h
+++ include/vers_id.h
@@ -3,7 +3,7 @@
 
 //#ifndef HAVE_CONFIG_H
 #	ifndef VERSION
-#		define VERSION		"1.18.33"
+#		define VERSION		"1.18.34"
 #	endif
 //#endif
 
@@ -16,7 +16,7 @@
 #	define D2X_MINOR	18
 #endif
 #ifndef D2X_MICRO
-#	define D2X_MICRO	33
+#	define D2X_MICRO	34
 #endif
 
 #define VERSION_TYPE		"Full Version"
