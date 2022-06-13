8c209eec8ae31c98ef088416399e1b2dde23f9d5
Arne de Bruijn <mail@arnedebruijn.nl>
2022-06-13 18:06:02 +0200

    Disable sound thread

--- audio/soundthreads.cpp
+++ audio/soundthreads.cpp
@@ -1,3 +1,4 @@
+//#define USE_SOUND_THREAD
 #ifdef HAVE_CONFIG_H
 #	include <conf.h>
 #endif
@@ -14,6 +15,35 @@
 
 CSoundThreadInfo tiSound;
 
+void SoundTask (tSoundTask nTask)
+{
+if (nTask == stOpenAudio) {
+	audio.Setup (tiSound.fSlowDown);
+	audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32768) / 8, 1);
+	audio.SetVolumes ((gameConfig.nAudioVolume [0] * 32768) / 8, (gameConfig.nMidiVolume * 128) / 8);
+	}
+else if (nTask == stCloseAudio) {
+	audio.Shutdown ();
+	}
+else if (nTask == stReconfigureAudio) {
+	FreeAddonSounds ();
+	audio.Shutdown ();
+	audio.Setup (tiSound.fSlowDown);
+	LoadAddonSounds ();
+	if (tiSound.fSlowDown == 1.0f) {
+		songManager.SetPos (songManager.SlowDown () - songManager.Start () + 
+								  2 * (SDL_GetTicks () - songManager.SlowDown ()) / gameOpts->gameplay.nSlowMotionSpeedup);
+		songManager.SetSlowDown (0);
+		}
+	else {
+		songManager.SetSlowDown (SDL_GetTicks ());
+		songManager.SetPos (songManager.SlowDown () - songManager.Start ());
+		}
+	songManager.PlayLevelSong (missionManager.nCurrentLevel, 1, false);
+	}
+}
+
+#ifdef USE_SOUND_THREAD
 //------------------------------------------------------------------------------
 
 int32_t _CDECL_ SoundThread (void *pThreadId)
@@ -26,67 +56,54 @@ do {
 			return 0;
 			}
 		}
-	if (tiSound.nTask == stOpenAudio) {
-		audio.Setup (tiSound.fSlowDown);
-		audio.SetFxVolume ((gameConfig.nAudioVolume [1] * 32768) / 8, 1);
-		audio.SetVolumes ((gameConfig.nAudioVolume [0] * 32768) / 8, (gameConfig.nMidiVolume * 128) / 8);
-		}
-	else if (tiSound.nTask == stCloseAudio) {
-		audio.Shutdown ();
-		}
-	else if (tiSound.nTask == stReconfigureAudio) {
-		FreeAddonSounds ();
-		audio.Shutdown ();
-		audio.Setup (tiSound.fSlowDown);
-		LoadAddonSounds ();
-		if (tiSound.fSlowDown == 1.0f) {
-			songManager.SetPos (songManager.SlowDown () - songManager.Start () + 
-									  2 * (SDL_GetTicks () - songManager.SlowDown ()) / gameOpts->gameplay.nSlowMotionSpeedup);
-			songManager.SetSlowDown (0);
-			}
-		else {
-			songManager.SetSlowDown (SDL_GetTicks ());
-			songManager.SetPos (songManager.SlowDown () - songManager.Start ());
-			}
-		songManager.PlayLevelSong (missionManager.nCurrentLevel, 1, false);
-		}
+	SoundTask (tiSound.nTask);
 	tiSound.bExec = 0;
 	} while (!tiSound.bDone);
 tiSound.bDone = 0;
 return 0;
 }
+#endif
 
 //------------------------------------------------------------------------------
 
 void WaitForSoundThread (time_t nTimeout)
 {
+#ifdef USE_SOUND_THREAD
 time_t t0 = (nTimeout < 0) ? 0 : (time_t) SDL_GetTicks ();
 while (tiSound.pThread && tiSound.bExec && ((nTimeout < 0) || ((time_t) SDL_GetTicks () - t0 < nTimeout)))
 	G3_SLEEP (1);
+#endif
 }
 
 //------------------------------------------------------------------------------
 
 bool HaveSoundThread (void)
 {
+#ifdef USE_SOUND_THREAD
 return tiSound.pThread != NULL;
+#else
+return true;
+#endif
 }
 
 //------------------------------------------------------------------------------
 
 void CreateSoundThread (void)
 {
+#ifdef USE_SOUND_THREAD
 if (!tiSound.pThread) {
 	memset (&tiSound, 0, sizeof (tiSound));
 	tiSound.nId = 0;
 	tiSound.pThread = SDL_CreateThread (SoundThread, &tiSound.nId);
 	}
+#endif
 }
 
 //------------------------------------------------------------------------------
 
 void DestroySoundThread (void)
 {
+#ifdef USE_SOUND_THREAD
 if (tiSound.pThread) {
 	WaitForSoundThread (1000);
 	tiSound.bDone = 1;
@@ -98,12 +115,17 @@ if (tiSound.pThread) {
 		SDL_KillThread (tiSound.pThread);
 	tiSound.pThread = NULL;
 	}	
+#endif
 }
 
 //------------------------------------------------------------------------------
 
 int32_t StartSoundThread (tSoundTask nTask)
 {
+#ifndef USE_SOUND_THREAD
+SoundTask (nTask);
+return 1;
+#else
 #if 1
 if (tiSound.pThread) {
 #else
@@ -118,6 +140,7 @@ if (tiSound.pThread && gameData.appData.bUseMultiThreading [rtSound]) {
 	return 1;
 	}
 return 0;
+#endif
 }
 
 //------------------------------------------------------------------------------
