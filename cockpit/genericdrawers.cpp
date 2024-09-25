arbruijn/d2x-xl

commit c8272401be692f5822e16efe41bf11d860be5b6c

Prevent transparent line above cockpit energy num

cockpit/genericdrawers.cpp
@@ -1333,7 +1333,7 @@ if (gameOpts->render.cockpit.bHUD || (gameStates.render.cockpit.nType != CM_FULL
 	LoadTexture (i, 0, 0, true);
 	if (pBm->HasOverride ())
 		pBm = pBm->Override (-1);
-	ogl.m_states.nTransparencyLimit = 8;	//add transparency to black areas of palettized cockpits (namely the display windows)
+	ogl.m_states.nTransparencyLimit = 0;	//add transparency to black areas of palettized cockpits (namely the display windows)
 	pBm->SetTranspType (3);
 	pBm->SetupTexture (0, 1);
 	ogl.m_states.nTransparencyLimit = 0;

commit 3abb0cbddcd643c524221562678528c460895f10

Fix full cockpit camera views
Fixes: c8272401

cockpit/genericdrawers.cpp
@@ -1333,10 +1333,12 @@ if (gameOpts->render.cockpit.bHUD || (gameStates.render.cockpit.nType != CM_FULL
 	LoadTexture (i, 0, 0, true);
 	if (pBm->HasOverride ())
 		pBm = pBm->Override (-1);
-	ogl.m_states.nTransparencyLimit = 0;	//add transparency to black areas of palettized cockpits (namely the display windows)
+	ogl.m_states.nTransparencyLimit = 9;	//add transparency to black areas of palettized cockpits (namely the display windows)
+	ogl.m_states.nTransparencyStartY = y == 0 ? pBm->Height() * 2 / 3 : 0;
 	pBm->SetTranspType (3);
 	pBm->SetupTexture (0, 1);
 	ogl.m_states.nTransparencyLimit = 0;
+	ogl.m_states.nTransparencyStartY = 0;
 	CCanvas::Current ()->SetColorRGBi (WHITE_RGBA);
 	pBm->RenderScaled (0, y, -1, CCanvas::Current ()->Height () - y, I2X (1), 0, &CCanvas::Current ()->Color ());
 	CCanvas::Current ()->SetColorRGBi (BLACK_RGBA);

include/ogl_lib.h
@@ -202,6 +202,7 @@ class COglStates {
 		int32_t	nColorBits;
 		int32_t	nPreloadTextures;
 		uint8_t	nTransparencyLimit;
+		int32_t nTransparencyStartY;
 		GLint	nDepthBits;
 		GLint	nStencilBits;
 		GLint	nStereo;

ogl/ogl_texture.cpp
@@ -629,7 +629,8 @@ for (y = 0; y < m_info.h; y++) {
 							}
 						if ((r < ogl.m_states.nTransparencyLimit) &&
 							 (g < ogl.m_states.nTransparencyLimit) &&
-							 (b < ogl.m_states.nTransparencyLimit)) {
+							 (b < ogl.m_states.nTransparencyLimit) &&
+							 (y >= ogl.m_states.nTransparencyStartY)) {
 							m_info.format = ogl.m_states.nRGBAFormat;
 							goto restart;
 							}
@@ -667,7 +668,8 @@ for (y = 0; y < m_info.h; y++) {
 							}
 						if ((r < ogl.m_states.nTransparencyLimit) &&
 							 (g < ogl.m_states.nTransparencyLimit) &&
-							 (b < ogl.m_states.nTransparencyLimit))
+							 (b < ogl.m_states.nTransparencyLimit) &&
+							 (y >= ogl.m_states.nTransparencyStartY))
 							a = 0;
 						else if (bAutoTransp) {
 #if 0 //non-linear formula