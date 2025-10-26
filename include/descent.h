5dd9f1925386a23388c1690d85d79587404b4293
Arne de Bruijn <ar112@pdc.nl>
2025-10-26 10:56:24 +0100

    Move CRiftData class to a separate file

--- include/descent.h
+++ include/descent.h
@@ -1892,63 +1892,13 @@ class CMeshEdge {
 		int32_t Partial (void);
 	};
 
-//------------------------------------------------------------------------------
-
-#if OCULUS_RIFT
-#	include "OVR.h"
-#	include "timeout.h"
-#endif
-
-class CRiftData {
-	public:
-#if OCULUS_RIFT
-		OVR::Ptr<OVR::DeviceManager>			m_pManager;
-		OVR::Ptr<OVR::HMDDevice>				m_pHMD;
-		OVR::Ptr<OVR::SensorDevice>			m_pSensorP;
-		OVR::HMDInfo								m_hmdInfo;
-		OVR::SensorFusion*						m_pSensorFusion;
-		OVR::Util::Render::StereoConfig		m_stereoConfig;
-		OVR::Util::Render::StereoEyeParams	m_eyes [2];
-#	if 0 // manual calibration removed from Rift SDK since v0.25
-		OVR::Util::MagCalibration				m_magCal;
-		CTimeout			m_magCalTO;
-		bool				m_bCalibrating;
-#	endif
-#endif
-
-		float				m_renderScale;
-		float				m_fov;
-		float				m_projectionCenterOffset;
-		int32_t			m_ipd;
-		int32_t			m_nResolution;
-		int32_t			m_bUse;
-		int32_t			m_bAvailable;
-		CFloatVector	m_center;
-
-		CRiftData () : m_renderScale (1.0f), m_fov (125.0f), m_projectionCenterOffset (0.0f), m_ipd (0), m_nResolution (0), m_bUse (0), m_bAvailable (false) {}
-		~CRiftData () { Destroy (); }
-		bool Create (void);
-		void Destroy (void);
-		inline int32_t Available (void) { return m_bAvailable; }
-		inline int32_t Resolution (void) { return m_nResolution; }
-		int32_t GetViewMatrix (CFixMatrix& m);
-		int32_t GetHeadAngles (CAngleVector* angles);
-		void AutoCalibrate (void);
-		inline void SetCenter (void) { GetHeadAngles (NULL); }
-#if OCULUS_RIFT
-		inline int32_t HResolution (void) { return (Available () &&  m_hmdInfo.HResolution) ? m_hmdInfo.HResolution : 1920; }
-		inline int32_t VResolution (void) { return (Available () && m_hmdInfo.VResolution) ? m_hmdInfo.VResolution : 1200; }
-#else
-		inline int32_t HResolution (void) { return 1920; }
-		inline int32_t VResolution (void) { return 1200; }
-#endif
-	};
-
 #define STEREO_OFFSET_NONE			0
 #define STEREO_OFFSET_FIXED		1
 #define STEREO_OFFSET_FLOATING	2
 
 #include "sphere.h"
+
+#include "riftdata.h"
 
 class CRenderData {
 	public:
---
+++ main/riftdata.cpp
@@ -0,0 +1,179 @@
+#ifdef HAVE_CONFIG_H
+#include <conf.h>
+#endif
+
+#include "descent.h"
+
+// ----------------------------------------------------------------------------
+
+bool CRiftData::Create (void)
+{
+#if OCULUS_RIFT
+gameData.renderData.rift.m_bAvailable = 0;
+if (gameOpts->render.bUseRift) {
+	OVR::System::Init (OVR::Log::ConfigureDefaultLog (OVR::LogMask_All));
+	m_pManager = *OVR::DeviceManager::Create();
+	if (m_pManager) {
+		//m_pManager->SetMessageHandler(this);
+
+		// Release Sensor/HMD in case this is a retry.
+		m_pSensorP.Clear ();
+		m_pHMD.Clear ();
+		// RenderParams.MonitorName.Clear();
+		m_pHMD = *m_pManager->EnumerateDevices<OVR::HMDDevice> ().CreateDevice ();
+		if (m_pHMD) {
+			m_pSensorP = *m_pHMD->GetSensor ();
+
+			// This will initialize m_hmdInfo with information about configured IPD,
+			// screen size and other variables needed for correct projection.
+			// We pass HMD DisplayDeviceName into the renderer to select the
+			// correct monitor in full-screen mode.
+			if (m_pHMD->GetDeviceInfo (&m_hmdInfo))	{            
+				// RenderParams.MonitorName = m_hmdInfo.DisplayDeviceName;
+				// RenderParams.DisplayId = m_hmdInfo.DisplayId;
+				m_stereoConfig.SetHMDInfo (m_hmdInfo);
+				m_stereoConfig.SetDistortionFitPointVP (-1, 0);
+				m_renderScale = m_stereoConfig.GetDistortionScale (); 
+				m_eyes [0] = m_stereoConfig.GetEyeRenderParams (OVR::Util::Render::StereoEye_Left);
+				m_eyes [1] = m_stereoConfig.GetEyeRenderParams (OVR::Util::Render::StereoEye_Right);
+				m_fov = m_stereoConfig.GetYFOVDegrees ();
+				float viewCenter = m_hmdInfo.HScreenSize * 0.25f;
+				float eyeProjectionShift = viewCenter - m_hmdInfo.LensSeparationDistance * 0.5f;
+				m_projectionCenterOffset = 4.0f * eyeProjectionShift / m_hmdInfo.HScreenSize;
+				}
+			}
+		else {            
+			// If we didn't detect an HMD, try to create the sensor directly.
+			// This is useful for debugging sensor interaction; it is not needed in
+			// a shipping app.
+			m_pSensorP = *m_pManager->EnumerateDevices<OVR::SensorDevice> ().CreateDevice ();
+			}
+		}
+	m_nResolution = HResolution () > 1280;
+	// If there was a problem detecting the Rift, display appropriate message.
+
+	const char* detectionMessage = NULL;
+
+	if (!m_pManager)
+		detectionMessage = "Cannot initialize Oculus Rift system.";
+	if (!m_pHMD && !m_pSensorP)
+		detectionMessage = "Oculus Rift not detected.";
+	else if (!m_pHMD)
+		detectionMessage = "Oculus Sensor detected; HMD Display not detected.\n";
+	else if (m_hmdInfo.DisplayDeviceName [0] == '\0')
+		detectionMessage = "Oculus Sensor detected; HMD display EDID not detected.\n";
+	else if (!m_pSensorP) {
+		m_bAvailable = 1;
+		detectionMessage = "Oculus HMD Display detected; Sensor not detected.\n";
+		}
+	else {
+		m_pSensorFusion = NEW OVR::SensorFusion;
+		m_pSensorFusion->AttachToSensor (m_pSensorP);
+		m_pSensorFusion->SetYawCorrectionEnabled (true);
+#if 0
+		m_magCalTO.Setup (60000); // 1 minute
+		m_magCalTO.Start (-1, true);
+		m_bCalibrating = false;
+#endif
+		m_bAvailable = 2;
+		}
+if (detectionMessage) 
+		PrintLog (0, detectionMessage);
+	}
+#endif
+return m_bAvailable != 0;
+}
+
+// ----------------------------------------------------------------------------
+
+int32_t CRiftData::GetViewMatrix (CFixMatrix& mOrient)
+{
+#if OCULUS_RIFT
+if (Available () < 2)
+	return 0;
+OVR::Quatf q = m_pSensorFusion->GetOrientation ();
+OVR::Matrix4f m (q);
+for (int32_t i = 0; i < 3; i++)
+	for (int32_t j = 0; j < 3; j++)
+		mOrient.m.vec [i * 3 + j] = F2X (m.M [i][j]);
+#endif
+return 1;
+}
+
+// ----------------------------------------------------------------------------
+
+#if OCULUS_RIFT
+static inline float AddDeadzone (float v)
+{
+float deadzone = float (gameOpts->input.oculusRift.nDeadzone) * 0.5f;
+
+if ((deadzone <= 0.0f) || (deadzone >= 1.0f))
+	return v;
+
+	float h = 1.0f / (1.0f - deadzone) - 1.0f;
+
+if (v < -deadzone)
+	return (v + deadzone) * (1.0f + h * fabs (v));
+if (v > deadzone)
+	return (v - deadzone) * (1.0f + h * fabs (v));
+return 0.0f;
+}
+#endif
+
+int32_t CRiftData::GetHeadAngles (CAngleVector* angles)
+{
+#if OCULUS_RIFT
+if (Available () < 2)
+	return 0;
+OVR::Quatf q = m_pSensorFusion->GetOrientation ();
+float yaw, pitch, roll;
+q.GetEulerAngles<OVR::Axis_Y, OVR::Axis_X, OVR::Axis_Z>(&yaw, &pitch, &roll);
+if (!angles) 
+	m_center.Set (pitch, roll, yaw);
+else {
+	pitch -= m_center.v.coord.x;
+	roll -= m_center.v.coord.y;
+	yaw -= m_center.v.coord.z;
+	angles->Set (-F2X (AddDeadzone (pitch) * 0.5f), F2X (AddDeadzone (roll) * 0.5f), -F2X (AddDeadzone (yaw) * 0.5f));
+	}
+#endif
+return 1;
+}
+
+// ----------------------------------------------------------------------------
+
+void CRiftData::AutoCalibrate (void)
+{
+#if 0
+if (Available () > 1) {
+	if (m_bCalibrating) {
+		if (!m_magCal.IsAutoCalibrating ()) {
+			m_bCalibrating = false;
+			m_magCalTO.Start (-1, true);
+			}
+		else {
+			m_magCal.UpdateAutoCalibration (*m_pSensorFusion);
+			if (m_magCal.IsCalibrated ()) {
+				m_bCalibrating = false;
+				m_magCalTO.Start (-1);
+				}
+			}
+		}
+	else if (m_magCalTO.Expired (false))
+		m_magCal.BeginAutoCalibration (*m_pSensorFusion);
+	}
+#endif
+}
+
+// ----------------------------------------------------------------------------
+
+void CRiftData::Destroy (void)
+{
+#if OCULUS_RIFT
+if (m_pSensorFusion) {
+	delete m_pSensorFusion;
+	m_pSensorFusion = NULL;
+	}
+#endif
+}
+
